31f03ff9b63db8f758793feb09bd481da4821a33
Mike Primm <mike@primmhome.com>
2014-03-14 16:30:00 -0500

    Rework custom 'furnace' - 2 types per block ID, vs 2 ids per type

--- common/com/westeroscraft/westerosblocks/blocks/WCFurnaceBlock.java
+++ common/com/westeroscraft/westerosblocks/blocks/WCFurnaceBlock.java
@@ -1,6 +1,5 @@
 package com.westeroscraft.westerosblocks.blocks;
 
-import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -13,13 +12,16 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityClientPlayerMP;
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.inventory.ContainerFurnace;
 import net.minecraft.item.ItemStack;
 import net.minecraft.network.INetworkManager;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.tileentity.TileEntityFurnace;
 import net.minecraft.util.Icon;
+import net.minecraft.util.MathHelper;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.common.ForgeDirection;
@@ -28,7 +30,6 @@ import com.westeroscraft.westerosblocks.WesterosBlockDef;
 import com.westeroscraft.westerosblocks.WesterosBlockDynmapSupport;
 import com.westeroscraft.westerosblocks.WesterosBlockLifecycle;
 import com.westeroscraft.westerosblocks.WesterosBlockFactory;
-import com.westeroscraft.westerosblocks.WesterosBlocks;
 import com.westeroscraft.westerosblocks.WesterosBlocksMessageDest;
 import com.westeroscraft.westerosblocks.WesterosBlocksPacketHandler;
 
@@ -37,67 +38,47 @@ import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
+// Custom furnace block
+// Distinct meta mapping, to allow 2 custom furnaces per block ID
+//  bit 0 = which custom furnace (0=first, 1=second)
+//  bit 1-2 = orientation (same as standard, but minus 2 on value (0-3 vs 2-5)
+//  bit 3 = active (1) vs idle (0)
 public class WCFurnaceBlock extends BlockFurnace implements WesterosBlockLifecycle, WesterosBlockDynmapSupport, WesterosBlocksMessageDest {
 
     public static class Factory extends WesterosBlockFactory {
         @Override
         public Block[] buildBlockClasses(WesterosBlockDef def) {
-            // Limit to 0
-            def.setMetaMask(0x0);
-            if (!def.validateMetaValues(new int[] { 0 }, null)) {
+            // Limit to 0, 1
+            def.setMetaMask(0x1);
+            if (!def.validateMetaValues(new int[] { 0, 1 }, null)) {
                 return null;
-            }
-            if (def.blockIDs.length != 2) {
-                WesterosBlocks.log.severe(String.format("Block '%s' does not have a valid number of blockIDs: 2 are required", def.blockName));
-                return null;
-            }
-            if (keepFurnaceInvetoryField == null) {
-                try {
-                    keepFurnaceInvetoryField = BlockFurnace.class.getField("field_72288_c");
-                    keepFurnaceInvetoryField.setAccessible(true);
-                } catch (NoSuchFieldException fnfx) {
-                    WesterosBlocks.log.severe(String.format("Block '%s' - fannot find keepFurnaceInventory", def.blockName));
-                    return null;
-                }
             }
             GameRegistry.registerTileEntity(WCFurnaceTileEntity.class, def.blockName);
 
-            WCFurnaceBlock inactive = new WCFurnaceBlock(def, false);
-            WCFurnaceBlock active = new WCFurnaceBlock(def, true);
-            inactive.otherBlock = active;
-            active.otherBlock = inactive;
-            return new Block[] { inactive, active };
+            return new Block[] { new WCFurnaceBlock(def) };
         }
     }
 
-    private static final int INACTIVE_IDX = 0;
-    private static final int ACTIVE_IDX = 1;
-    
     private WesterosBlockDef def;
-    private WCFurnaceBlock otherBlock;
-    private boolean active;
-    private boolean alwaysOn = false;
-    private static Field keepFurnaceInvetoryField;
+    private boolean alwaysOn[] = new boolean[2];
     
-    protected WCFurnaceBlock(WesterosBlockDef def, boolean is_active) {
-        super(def.blockIDs[is_active?ACTIVE_IDX:INACTIVE_IDX], is_active);
+    protected WCFurnaceBlock(WesterosBlockDef def) {
+        super(def.blockID, false);
         this.def = def;
-        this.active = is_active;
-        def.doStandardContructorSettings(this, is_active?ACTIVE_IDX:INACTIVE_IDX);
+        def.doStandardContructorSettings(this);
         if (def.lightOpacity < 0) {
             this.setLightOpacity(0);    // Workaround MC's f*cked up lighting exceptions
         }
-        if (is_active) {
-            this.setCreativeTab(null);
-        }
-        String type = def.getType(0);
-        if (type != null) {
-            String[] toks = type.split(",");
-            for (String tok : toks) {
-                String [] flds = tok.split(":");
-                if (flds.length < 2) continue;
-                if (flds[0].equals("always-on")) {
-                    alwaysOn = flds[1].equals("true");
+        for (int i = 0; i < 2; i++) {
+            String type = def.getType(i);
+            if (type != null) {
+                String[] toks = type.split(",");
+                for (String tok : toks) {
+                    String [] flds = tok.split(":");
+                    if (flds.length < 2) continue;
+                    if (flds[0].equals("always-on")) {
+                        alwaysOn[i] = flds[1].equals("true");
+                    }
                 }
             }
         }
@@ -110,13 +91,7 @@ public class WCFurnaceBlock extends BlockFurnace implements WesterosBlockLifecyc
     }
 
     public boolean registerBlockDefinition() {
-        if (this.active) {
-            def.doStandardRegisterActions(this, MultiBlockItem.class, null, ACTIVE_IDX);
-        }
-        else {
-            def.doStandardRegisterActions(this, MultiBlockItem.class, null, INACTIVE_IDX);
-        }
-        
+        def.doStandardRegisterActions(this, MultiBlockItem.class);
         return true;
     }
 
@@ -127,17 +102,51 @@ public class WCFurnaceBlock extends BlockFurnace implements WesterosBlockLifecyc
         def.doStandardRegisterIcons(iconRegister);
     }
 
+    
+    /**
+     * Called whenever the block is added into the world. Args: world, x, y, z
+     */
+    public void onBlockAdded(World world, int x, int y, int z)
+    {
+        if (!world.isRemote) {
+            int meta = world.getBlockMetadata(x, y, z);    // Save meta (need low bit)
+            int zn_id = world.getBlockId(x, y, z - 1);
+            int zp_id = world.getBlockId(x, y, z + 1);
+            int xn_id = world.getBlockId(x - 1, y, z);
+            int xp_id = world.getBlockId(x + 1, y, z);
+            byte dir = 1;
+
+            if (Block.opaqueCubeLookup[zn_id] && !Block.opaqueCubeLookup[zp_id]) {
+                dir = 1;
+            }
+
+            if (Block.opaqueCubeLookup[zp_id] && !Block.opaqueCubeLookup[zn_id]) {
+                dir = 0;
+            }
+
+            if (Block.opaqueCubeLookup[xn_id] && !Block.opaqueCubeLookup[xp_id]) {
+                dir = 3;
+            }
+
+            if (Block.opaqueCubeLookup[xp_id] && !Block.opaqueCubeLookup[xn_id]) {
+                dir = 2;
+            }
+            world.setBlockMetadataWithNotify(x, y, z, (dir << 1) | (meta & 0x1), 2);
+        }
+    }
+
     @Override
     @SideOnly(Side.CLIENT)
     public Icon getIcon(int side, int meta) {
         int idx;
+        int metaside = ((meta >> 1) & 0x3) + 2;
         if (side < 2) { // Top/bottom
             idx = side;
         }
-        else if (meta != side) {
+        else if (metaside != side) {
             idx = 2;    // Side/back
         }
-        else if (active) {
+        else if ((meta & 0x8) != 0) {
             idx = 3;    // Active furnace
         }
         else {
@@ -173,7 +182,9 @@ public class WCFurnaceBlock extends BlockFurnace implements WesterosBlockLifecyc
     }
     @Override
     public int getLightValue(IBlockAccess world, int x, int y, int z) {
-        if (active || alwaysOn) {
+        int meta = world.getBlockMetadata(x, y, z);
+        boolean active = alwaysOn[meta & 0x1] || ((meta & 0x8) != 0);
+        if (active) {
             return def.getLightValue(world, x, y, z);
         }
         return 0;
@@ -201,27 +212,18 @@ public class WCFurnaceBlock extends BlockFurnace implements WesterosBlockLifecyc
     }
     @Override
     public int idDropped(int meta, Random rnd, int par3) {
-        if (this.active) {
-            return this.otherBlock.blockID;
-        }
-        else {
-            return blockID;
-        }
+        return this.blockID;
     }
     @Override
     public int idPicked(World world, int x, int y, int z) {
-        if (!active) {
-            return blockID;
-        }
-        else {
-            return otherBlock.blockID;
-        }
+        return this.blockID;
     }
 
     @Override
-    protected ItemStack createStackedBlock(int meta) {
-        return new ItemStack(blockID, 2, meta);
+    public int damageDropped(int meta) {
+        return meta & 0x1;
     }
+
     @SideOnly(Side.CLIENT)
     public int getRenderBlockPass()
     {
@@ -230,37 +232,39 @@ public class WCFurnaceBlock extends BlockFurnace implements WesterosBlockLifecyc
     
     @SideOnly(Side.CLIENT)
     @Override
-    public void randomDisplayTick(World par1World, int par2, int par3, int par4, Random par5Random) {
-        def.doRandomDisplayTick(par1World, par2, par3, par4, par5Random);
+    public void randomDisplayTick(World world, int x, int y, int z, Random random) {
+        def.doRandomDisplayTick(world, x, y, z, random);
 
-        if (this.active || this.alwaysOn)
-        {
-            int l = par1World.getBlockMetadata(par2, par3, par4);
-            float f = (float)par2 + 0.5F;
-            float f1 = (float)par3 + 0.0F + par5Random.nextFloat() * 6.0F / 16.0F;
-            float f2 = (float)par4 + 0.5F;
+        int meta = world.getBlockMetadata(x, y, z);
+        boolean active = alwaysOn[meta & 0x1] || ((meta & 0x8) != 0);
+        
+        if (active) {
+            int dir = ((meta >> 1) & 0x3) + 2;  // Compute face we're facing
+            float f = (float)x + 0.5F;
+            float f1 = (float)y + 0.0F + random.nextFloat() * 6.0F / 16.0F;
+            float f2 = (float)z + 0.5F;
             float f3 = 0.52F;
-            float f4 = par5Random.nextFloat() * 0.6F - 0.3F;
+            float f4 = random.nextFloat() * 0.6F - 0.3F;
 
-            if (l == 4)
+            if (dir == 4)
             {
-                par1World.spawnParticle("smoke", (double)(f - f3), (double)f1, (double)(f2 + f4), 0.0D, 0.0D, 0.0D);
-                par1World.spawnParticle("flame", (double)(f - f3), (double)f1, (double)(f2 + f4), 0.0D, 0.0D, 0.0D);
+                world.spawnParticle("smoke", (double)(f - f3), (double)f1, (double)(f2 + f4), 0.0D, 0.0D, 0.0D);
+                world.spawnParticle("flame", (double)(f - f3), (double)f1, (double)(f2 + f4), 0.0D, 0.0D, 0.0D);
             }
-            else if (l == 5)
+            else if (dir == 5)
             {
-                par1World.spawnParticle("smoke", (double)(f + f3), (double)f1, (double)(f2 + f4), 0.0D, 0.0D, 0.0D);
-                par1World.spawnParticle("flame", (double)(f + f3), (double)f1, (double)(f2 + f4), 0.0D, 0.0D, 0.0D);
+                world.spawnParticle("smoke", (double)(f + f3), (double)f1, (double)(f2 + f4), 0.0D, 0.0D, 0.0D);
+                world.spawnParticle("flame", (double)(f + f3), (double)f1, (double)(f2 + f4), 0.0D, 0.0D, 0.0D);
             }
-            else if (l == 2)
+            else if (dir == 2)
             {
-                par1World.spawnParticle("smoke", (double)(f + f4), (double)f1, (double)(f2 - f3), 0.0D, 0.0D, 0.0D);
-                par1World.spawnParticle("flame", (double)(f + f4), (double)f1, (double)(f2 - f3), 0.0D, 0.0D, 0.0D);
+                world.spawnParticle("smoke", (double)(f + f4), (double)f1, (double)(f2 - f3), 0.0D, 0.0D, 0.0D);
+                world.spawnParticle("flame", (double)(f + f4), (double)f1, (double)(f2 - f3), 0.0D, 0.0D, 0.0D);
             }
-            else if (l == 3)
+            else if (dir == 3)
             {
-                par1World.spawnParticle("smoke", (double)(f + f4), (double)f1, (double)(f2 + f3), 0.0D, 0.0D, 0.0D);
-                par1World.spawnParticle("flame", (double)(f + f4), (double)f1, (double)(f2 + f3), 0.0D, 0.0D, 0.0D);
+                world.spawnParticle("smoke", (double)(f + f4), (double)f1, (double)(f2 + f3), 0.0D, 0.0D, 0.0D);
+                world.spawnParticle("flame", (double)(f + f4), (double)f1, (double)(f2 + f3), 0.0D, 0.0D, 0.0D);
             }
         }
     }
@@ -279,31 +283,22 @@ public class WCFurnaceBlock extends BlockFurnace implements WesterosBlockLifecyc
         return new WCFurnaceTileEntity();
     }
 
-    private static void setKeepFurnaceInventory(boolean k) {
-        try {
-            keepFurnaceInvetoryField.set(null, k);
-        } catch (IllegalArgumentException e) {
-        } catch (IllegalAccessException e) {
-        }
-    }
     /**
      * Update which block ID the furnace is using depending on whether or not it is burning
      */
     public static void updateFurnaceBlockState(boolean toActive, World world, int x, int y, int z)
     {
-        int l = world.getBlockMetadata(x, y, z);
-        int blkid = world.getBlockId(x, y, z);
-        WCFurnaceBlock blk = (WCFurnaceBlock) Block.blocksList[blkid];
+        int meta = world.getBlockMetadata(x, y, z);
         TileEntity tileentity = world.getBlockTileEntity(x, y, z);
-        setKeepFurnaceInventory(true);
-
-        if (toActive != blk.active) {   // Different state?
-            world.setBlock(x, y, z, blk.otherBlock.blockID);
-        }
-
-        setKeepFurnaceInventory(false);
         
-        world.setBlockMetadataWithNotify(x, y, z, l, 2);
+        if (toActive) {
+            meta |= 0x8;
+        }
+        else {
+            meta &= 0x7;
+        }
+        
+        world.setBlockMetadataWithNotify(x, y, z, meta, 2);
 
         if (tileentity != null)
         {
@@ -352,6 +347,36 @@ public class WCFurnaceBlock extends BlockFurnace implements WesterosBlockLifecyc
             }
 
             return true;
+        }
+    }
+
+    /**
+     * Called when the block is placed in the world.
+     */
+    @Override
+    public void onBlockPlacedBy(World world, int x, int y, int z, EntityLivingBase entity, ItemStack item)
+    {
+        int facing = MathHelper.floor_double((double)(entity.rotationYaw * 4.0F / 360.0F) + 0.5D) & 3;
+        int meta = item.getItemDamage() & 0x1;
+        switch (facing) {
+            case 0:
+                meta += (0 << 1);
+                break;
+            case 1:
+                meta += (3 << 1);
+                break;
+            case 2:
+                meta += (1 << 1);
+                break;
+            case 3:
+                meta += (2 << 1);
+                break;
+        }
+        world.setBlockMetadataWithNotify(x, y, z, meta, 2);
+
+        if (item.hasDisplayName())
+        {
+            ((TileEntityFurnace)world.getBlockTileEntity(x, y, z)).setGuiDisplayName(item.getDisplayName());
         }
     }
 
