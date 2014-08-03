c9435e942e8a4000714d922defdf2201186d7cc6
Mike Primm <mike@primmhome.com>
2014-08-03 14:57:43 -0500

    Add WesterosSounds tab

--- src/main/java/com/westeroscraft/westerosblocks/WesterosBlocksCreativeTab.java
+++ src/main/java/com/westeroscraft/westerosblocks/WesterosBlocksCreativeTab.java
@@ -7,33 +7,34 @@ import net.minecraft.item.ItemStack;
 
 public class  WesterosBlocksCreativeTab extends CreativeTabs {
 
-    public static final CreativeTabs tabWesterosBlocks = new  WesterosBlocksCreativeTab("WesterosBlocks", "Westeros Blocks", 0);
-    public static final CreativeTabs tabWesterosDecorative = new  WesterosBlocksCreativeTab("WesterosDeco", "Westeros Decorative", 1);
-    public static final CreativeTabs tabWesterosPlants = new  WesterosBlocksCreativeTab("WesterosPlants", "Westeros Plants", 2);
+    public static final CreativeTabs tabWesterosBlocks = new  WesterosBlocksCreativeTab("WesterosBlocks", "Westeros Blocks", "metal_block_0");
+    public static final CreativeTabs tabWesterosDecorative = new  WesterosBlocksCreativeTab("WesterosDeco", "Westeros Decorative", "metal_block_0_stair_1");
+    public static final CreativeTabs tabWesterosPlants = new  WesterosBlocksCreativeTab("WesterosPlants", "Westeros Plants", "yellow_flower_block_0");
+    public static final CreativeTabs tabWesterosSounds = new  WesterosBlocksCreativeTab("WesterosSounds", "Westeros Sounds", "sound_blocks_0");
     
     public static void init() {
         
     }
     
-    private int tab;
     private String lbl;
-    public  WesterosBlocksCreativeTab(String id, String label, int tabnum) {
+    private String type;
+    private Block blk = null;
+    public  WesterosBlocksCreativeTab(String id, String label, String type) {
         super(id);
-        tab = tabnum;
         lbl = label;
+        this.type = type;
         WesterosBlockDef.addCreativeTab(id,  this);
     }
 
     @Override
     public ItemStack getIconItemStack() {
-        switch (tab) {
-            case 0:
-                return new ItemStack(WesterosBlocks.customBlocks[0], 1, 0);
-            case 1:
-                return new ItemStack(WesterosBlocks.customBlocks[1], 1, 0);
-            default:
-                return new ItemStack(WesterosBlocks.customBlocks[2], 1, 0);
+        if (blk == null) {
+            blk = WesterosBlocks.findBlockByName(this.type);
+            if (blk == null) {
+                blk = WesterosBlocks.customBlocks[0];
+            }
         }
+        return new ItemStack(blk, 1, 0);
     }
     @Override
     public String getTranslatedTabLabel() {
