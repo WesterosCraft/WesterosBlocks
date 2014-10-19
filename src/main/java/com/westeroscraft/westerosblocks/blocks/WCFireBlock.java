67ededf1a1935ba7724e6b5741af45d2f1219232
Mike Primm <mike@primmhome.com>
2014-10-19 10:28:47 -0500

    Add dynmap render for fire blocks

--- src/main/java/com/westeroscraft/westerosblocks/blocks/WCFireBlock.java
+++ src/main/java/com/westeroscraft/westerosblocks/blocks/WCFireBlock.java
@@ -2,14 +2,22 @@ package com.westeroscraft.westerosblocks.blocks;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+
 import java.util.Random;
 
+import org.dynmap.modsupport.BlockTextureRecord;
+import org.dynmap.modsupport.ModModelDefinition;
 import org.dynmap.modsupport.ModTextureDefinition;
+import org.dynmap.modsupport.PatchBlockModel;
+import org.dynmap.modsupport.TextureModifier;
+import org.dynmap.modsupport.TransparencyMode;
+import org.dynmap.renderer.RenderPatchFactory.SideVisible;
 
 import com.westeroscraft.westerosblocks.WesterosBlockDef;
 import com.westeroscraft.westerosblocks.WesterosBlockDynmapSupport;
 import com.westeroscraft.westerosblocks.WesterosBlockFactory;
 import com.westeroscraft.westerosblocks.WesterosBlockLifecycle;
+import com.westeroscraft.westerosblocks.WesterosBlockDef.Subblock;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockFire;
@@ -17,7 +25,6 @@ import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.util.Icon;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
-
 import net.minecraftforge.common.ForgeDirection;
 
 public class WCFireBlock extends BlockFire implements WesterosBlockLifecycle, WesterosBlockDynmapSupport
@@ -122,8 +129,37 @@ public class WCFireBlock extends BlockFire implements WesterosBlockLifecycle, We
 
     @Override
     public void registerDynmapRenderData(ModTextureDefinition mtd) {
-        // TODO Auto-generated method stub
-        
+        ModModelDefinition md = mtd.getModelDefinition();
+        def.defaultRegisterTextures(mtd);
+        // Register textures
+        Subblock sb = def.getByMeta(0);
+        if ((sb == null) || (sb.textures == null) || (sb.textures.size() < 2)) return;
+        String txt1 = sb.textures.get(0);
+        String txt2 = sb.textures.get(1);
+        BlockTextureRecord btr = mtd.addBlockTextureRecord(this.blockID);
+        btr.setTransparencyMode(TransparencyMode.TRANSPARENT);
+        btr.setPatchTexture(txt1, TextureModifier.NONE, 0);
+        btr.setPatchTexture(txt1, TextureModifier.NONE, 1);
+        btr.setPatchTexture(txt1, TextureModifier.NONE, 2);
+        btr.setPatchTexture(txt1, TextureModifier.NONE, 3);
+        btr.setPatchTexture(txt2, TextureModifier.NONE, 4);
+        btr.setPatchTexture(txt2, TextureModifier.NONE, 5);
+        btr.setPatchTexture(txt2, TextureModifier.NONE, 6);
+        btr.setPatchTexture(txt2, TextureModifier.NONE, 7);
+        def.setBlockColorMap(btr, sb);
+        /* Make base model */
+        PatchBlockModel mod = md.addPatchModel(this.blockID);
+        // patchblock:id=51,data=*,patch0=VertX0,patch1=VertX0@90,patch2=VertX0@180,patch3=VertX0@270,patch4=SlopeXUpZTop675,patch5=SlopeXUpZTop675@90,patch6=SlopeXUpZTop675@180,patch4=SlopeXUpZTop675@270
+        // patch:id=VertX0,Ox=0.0,Oy=0.0,Oz=1.0,Ux=0.0,Uy=0.0,Uz=0.0,Vx=0.0,Vy=1.0,Vz=1.0,visibility=bottom
+        // patch:id=SlopeXUpZTop675,Ox=0.375,Oy=0.0,Oz=0.0,Ux=0.375,Uy=0.0,Uz=1.0,Vx=0.5,Vy=1.0,Vz=0.0,visibility=top
+        String patch0 = mod.addPatch(0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, SideVisible.BOTH);
+        mod.addRotatedPatch(patch0, 0, 90, 0);
+        mod.addRotatedPatch(patch0, 0, 180, 0);
+        mod.addRotatedPatch(patch0, 0, 270, 0);
+        String patch4 = mod.addPatch(0.375, 0.0, 0.0, 0.375, 0.0, 1.0, 0.5, 1.0, 0.0, SideVisible.BOTH);
+        mod.addRotatedPatch(patch4, 0, 90, 0);
+        mod.addRotatedPatch(patch4, 0, 180, 0);
+        mod.addRotatedPatch(patch4, 0, 270, 0);
     }
 
     @Override
