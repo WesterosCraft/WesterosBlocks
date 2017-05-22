6c486c49528b26bfab3d41d4b69a468f6c425770
Mike Primm <mike@primmhome.com>
2017-05-21 23:39:11 -0500

    Add class transformer for fixing cloud height

---
+++ src/main/java/com/westeroscraft/westerosblocks/asm/ClassTransformer.java
@@ -0,0 +1,165 @@
+package com.westeroscraft.westerosblocks.asm;
+
+import java.util.Iterator;
+
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.Label;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.tree.AbstractInsnNode;
+import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.MethodNode;
+import org.objectweb.asm.Opcodes;
+
+import com.westeroscraft.westerosblocks.WesterosBlocks;
+
+import net.minecraft.launchwrapper.IClassTransformer;
+
+public class ClassTransformer implements IClassTransformer, Opcodes {
+    @Override
+    public byte[] transform(String name, String tname, byte[] bytes)
+    {
+        if (name.equals("ajv")) { // Obfuscated name for net.minecraft.world.WorldType
+            bytes = transformWorldType(name, bytes, true);
+        }
+        else if (name.equals("net.minecraft.world.WorldType")) {    // Clear name
+            bytes = transformWorldType(name, bytes, false);
+        }
+        return bytes;
+    }
+    
+    private MethodNode findMethod(ClassNode cls, String methname, String methsig) {
+        Iterator<MethodNode> methods = cls.methods.iterator();
+        while(methods.hasNext()) {
+            MethodNode m = methods.next();
+            // Check if method we want
+            if (m.name.equals(methname) && (m.desc.equals(methsig))) {
+                return m;
+            }
+        }
+        return null;
+    }
+    private int findOpSequence(MethodNode meth, int[] opcodes) {
+        Iterator<AbstractInsnNode> iter = meth.instructions.iterator();
+        int index = -1;
+        int seqindex = 0;
+        int seqstartindex = -1;
+
+        while (iter.hasNext()) {
+            index++;
+            AbstractInsnNode node = iter.next();
+            if (node.getOpcode() == opcodes[seqindex]) {
+                if (seqindex == 0) seqstartindex = index;   // Remember start
+                seqindex++;
+                if (seqindex == opcodes.length) {   // Done?
+                    return seqstartindex;
+                }
+            }
+            else {  // Mismatch
+                seqindex = 0;
+                seqstartindex = -1;
+            }
+        }
+        return -1;
+    }
+    
+    @SuppressWarnings("unused")
+    private void printMethod(MethodNode meth) {
+        Iterator<AbstractInsnNode> iter = meth.instructions.iterator();
+        System.out.println("printMethod(" + meth.name + ")");
+        while (iter.hasNext()) {
+            AbstractInsnNode n = iter.next();
+            System.out.println("  " + n.getOpcode() + " (" + n.getType() + " - " + n.getClass().getName() + ")");
+        }
+    }
+        
+    
+    private byte[] transformWorldType(String name, byte[] b, boolean obfus) {
+        String targetMethodName = "";
+        String targetMethodSig = "";
+
+        WesterosBlocks.log.fine("Checking class " + name);
+        
+        targetMethodName ="getCloudHeight";
+        targetMethodSig = "()F";
+        
+        //set up ASM class manipulation stuff. Consult the ASM docs for details
+        ClassNode classNode = new ClassNode();
+        ClassReader classReader = new ClassReader(b);
+        classReader.accept(classNode, 0);
+
+        // Now find the method
+        MethodNode m = findMethod(classNode, targetMethodName, targetMethodSig);
+        if (m == null) {
+            WesterosBlocks.log.warning("Cannot find "  + targetMethodName + "() in " + name + " for patching");
+            return b;
+        }
+        // Find our target op sequence
+        int ldx_index = findOpSequence(m, new int[] { LDC } );
+        if (ldx_index < 0) {
+            WesterosBlocks.log.warning("Cannot patch "  + targetMethodName + "() in " + name);
+            return b;
+        }
+        // Replace method implementation
+        MethodVisitor mv = m;
+        m.instructions.clear();
+        m.localVariables.clear();
+
+        mv.visitCode();
+        Label l0 = new Label();
+        mv.visitLabel(l0);
+        mv.visitLineNumber(11, l0);
+        mv.visitVarInsn(ALOAD, 0);
+        if (obfus) {
+            mv.visitFieldInsn(GETFIELD, "ajv", "f", "I");
+        }
+        else {
+            mv.visitFieldInsn(GETFIELD, "net/minecraft/world/WorldType", "worldTypeId", "I");
+        }
+        mv.visitMethodInsn(INVOKESTATIC, "com/westeroscraft/westerosblocks/asm/ClassTransformer", "getWorldHeight", "(I)F");
+        mv.visitVarInsn(FSTORE, 1);
+        Label l1 = new Label();
+        mv.visitLabel(l1);
+        mv.visitLineNumber(12, l1);
+        mv.visitVarInsn(FLOAD, 1);
+        mv.visitInsn(FCONST_0);
+        mv.visitInsn(FCMPL);
+        Label l2 = new Label();
+        mv.visitJumpInsn(IFLE, l2);
+        Label l3 = new Label();
+        mv.visitLabel(l3);
+        mv.visitLineNumber(13, l3);
+        mv.visitVarInsn(FLOAD, 1);
+        mv.visitInsn(FRETURN);
+        mv.visitLabel(l2);
+        mv.visitLineNumber(15, l2);
+        mv.visitLdcInsn(new Float("128.0"));
+        mv.visitInsn(FRETURN);
+        Label l4 = new Label();
+        mv.visitLabel(l4);
+        if (obfus) {
+            mv.visitLocalVariable("this", "Lajv;", null, l0, l4, 0);
+        }
+        else {
+            mv.visitLocalVariable("this", "Lnet/minecraft/world/WorldType;", null, l0, l4, 0);
+        }
+        mv.visitLocalVariable("rslt", "F", null, l1, l4, 1);
+        mv.visitMaxs(2, 2);
+        mv.visitEnd();
+
+        //ASM specific for cleaning up and returning the final bytes for JVM processing.
+        ClassWriter writer = new ClassWriter(ClassWriter.COMPUTE_MAXS);
+        classNode.accept(writer);
+        b = writer.toByteArray();
+        
+        WesterosBlocks.log.info("Method " + targetMethodName + "() of " + name + " patched!");
+        
+        return b;
+    }
+    
+    public static float getWorldHeight(int wtIndex) {
+        return 256.0F;
+    }
+    
+}
+
