e5a17d8292aa4a932f1bbae724df8948bee79124
Mike Primm <mprimm@trivatec.com>
2021-08-08 21:05:47 -0500

    Fix copy script

--- copyFiles.sh
+++ copyFiles.sh
@@ -1,5 +1,10 @@
+mkdir -p src/main/resources/assets/westerosblocks/lang
+mkdir -p src/main/resources/assets/westerosblocks/models/block/generated
+mkdir -p src/main/resources/assets/westerosblocks/models/item
+mkdir -p src/main/resources/assets/westerosblocks/blockstates
+mkdir -p src/main/resources/data/minecraft/tags/blocks
 cp ~/forge-1.16.5/config/westerosblocks/assets/westerosblocks/lang/*.json src/main/resources/assets/westerosblocks/lang
 cp ~/forge-1.16.5/config/westerosblocks/assets/westerosblocks/blockstates/*.json src/main/resources/assets/westerosblocks/blockstates
 cp ~/forge-1.16.5/config/westerosblocks/assets/westerosblocks/models/block/generated/*.json src/main/resources/assets/westerosblocks/models/block/generated
-cp ~/forge-1.16.5/config/westerosblocks/assets/westerosblocks/models/item//generated/*.json src/main/resources/assets/westerosblocks/models/item
+cp ~/forge-1.16.5/config/westerosblocks/assets/westerosblocks/models/item//*.json src/main/resources/assets/westerosblocks/models/item
 cp ~/forge-1.16.5/config/westerosblocks/data/minecraft/tags/blocks/*.json src/main/resources/data/minecraft/tags/blocks
