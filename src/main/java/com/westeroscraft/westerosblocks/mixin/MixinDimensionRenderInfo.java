8ac2952081e28a1a8dfbaa64554e88cb45e2120f
Mike Primm <mprimm@trivatec.com>
2021-08-20 03:10:31 -0500

    More mixin changes

--- src/main/java/com/westeroscraft/westerosblocks/mixin/MixinDimensionRenderInfo.java
+++ src/main/java/com/westeroscraft/westerosblocks/mixin/MixinDimensionRenderInfo.java
@@ -1,12 +1,10 @@
 package com.westeroscraft.westerosblocks.mixin;
 
+import net.minecraft.client.world.DimensionRenderInfo;
 import org.spongepowered.asm.mixin.Mixin;
 
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraft.client.world.DimensionRenderInfo;
 
-@Mixin(DimensionRenderInfo.Overworld.class) 
+@Mixin(DimensionRenderInfo.class) 
 public abstract class MixinDimensionRenderInfo
 {	
 	// This constructor is fake and never used
