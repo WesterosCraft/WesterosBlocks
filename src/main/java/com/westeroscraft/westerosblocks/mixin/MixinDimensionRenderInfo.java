99fc88cbe5951388fad75fa0af3889a620c2c778
Mike Primm <mprimm@trivatec.com>
2021-08-20 18:48:13 -0500

    Fix cloud height

--- src/main/java/com/westeroscraft/westerosblocks/mixin/MixinDimensionRenderInfo.java
+++ src/main/java/com/westeroscraft/westerosblocks/mixin/MixinDimensionRenderInfo.java
@@ -2,19 +2,26 @@ package com.westeroscraft.westerosblocks.mixin;
 
 import net.minecraft.client.world.DimensionRenderInfo;
 import org.spongepowered.asm.mixin.Mixin;
-
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(DimensionRenderInfo.class) 
 public abstract class MixinDimensionRenderInfo
-{	
+{	@Shadow float cloudLevel;
+
 	// This constructor is fake and never used
 	protected MixinDimensionRenderInfo()
 	{
 	}
-	
-	// Replace method
-	public float getCloudHeight()
-	{
-		return 255.0f;
+
+	@Inject(method = "getCloudHeight", at = @At("TAIL"), cancellable=true)	
+	private void doGetCloudHeight(CallbackInfoReturnable<Float> ci) {
+		if (cloudLevel == 128.0F) {
+			ci.setReturnValue(255.0F);
+		}
 	}
+
 }
