78a51366f490567925633086660651af24fe4a3f
Mike Primm <mprimm@trivatec.com>
2021-08-29 14:19:24 -0500

    Add condStates to plant blocks, migrate cranberry CTM

--- src/main/java/com/westeroscraft/westerosblocks/blocks/WCPlantBlock.java
+++ src/main/java/com/westeroscraft/westerosblocks/blocks/WCPlantBlock.java
@@ -8,14 +8,18 @@ import org.dynmap.renderer.RenderPatchFactory.SideVisible;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockReader;
 import net.minecraftforge.common.IPlantable;
+import net.minecraft.state.StateContainer;
 
 import com.westeroscraft.westerosblocks.WesterosBlockDef;
 import com.westeroscraft.westerosblocks.WesterosBlockDynmapSupport;
 import com.westeroscraft.westerosblocks.WesterosBlockLifecycle;
 import com.westeroscraft.westerosblocks.WesterosBlockFactory;
+
+import javax.annotation.Nullable;
 
 public class WCPlantBlock extends Block implements WesterosBlockLifecycle, IPlantable, WesterosBlockDynmapSupport {
 
@@ -23,18 +27,51 @@ public class WCPlantBlock extends Block implements WesterosBlockLifecycle, IPlan
         @Override
         public Block buildBlockClass(WesterosBlockDef def) {
         	AbstractBlock.Properties props = def.makeProperties().noCollission().instabreak();
+        	// See if we have a cond property
+        	WesterosBlockDef.CondProperty prop = def.buildCondProperty();
+        	if (prop != null) {
+        		tempCOND = prop;
+        	}        	
+
         	return def.registerRenderType(def.registerBlock(new WCPlantBlock(props, def)), false, false);
         }
     }    
     private WesterosBlockDef def;
+    protected static WesterosBlockDef.CondProperty tempCOND;
+    protected WesterosBlockDef.CondProperty COND;
 
     protected WCPlantBlock(AbstractBlock.Properties props, WesterosBlockDef def) {
         super(props);
         this.def = def;
+        if (COND != null) {
+            this.registerDefaultState(this.stateDefinition.any().setValue(COND, COND.defValue));
+        }
     }
     @Override
     public WesterosBlockDef getWBDefinition() {
         return def;
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateContainer.Builder<Block, BlockState> stateContainer) {
+    	super.createBlockStateDefinition(stateContainer);
+    	if (tempCOND != null) {
+    		COND = tempCOND;
+    		tempCOND = null;
+    	}
+    	if (COND != null) {
+	       stateContainer.add(COND);
+    	}
+    }
+
+    @Override
+    @Nullable
+    public BlockState getStateForPlacement(BlockItemUseContext ctx) {
+    	BlockState bs = super.getStateForPlacement(ctx);
+    	if ((COND != null) && (bs != null)) {
+    		bs = bs.setValue(COND, def.getMatchingCondition(ctx.getLevel(), ctx.getClickedPos())); 
+    	}
+    	return bs;
     }
 
     @Override
@@ -49,6 +86,7 @@ public class WCPlantBlock extends Block implements WesterosBlockLifecycle, IPlan
         mod.addRotatedPatch(patch0, 0, 90, 0);
         mod.setMetaValue(0);
     }
+    
 	@Override
 	public BlockState getPlant(IBlockReader world, BlockPos pos) {
 		BlockState state = world.getBlockState(pos);
