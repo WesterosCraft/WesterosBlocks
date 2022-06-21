dbfb475517efd0218d11fc34df06c28d9c7e9da5
jacob.granberry <jacobgranberry@gmail.com>
2022-06-21 14:58:04 -0500

    changes pizza copy file

--- copyFilesPizza.sh
+++ copyFilesPizza.sh
@@ -4,8 +4,9 @@ mkdir -p src/main/resources/assets/westerosblocks/models/block/generated
 mkdir -p src/main/resources/assets/westerosblocks/models/item
 mkdir -p src/main/resources/assets/westerosblocks/blockstates
 mkdir -p src/main/resources/data/minecraft/tags/blocks
-cp ../../../Desktop/minecraft_servers/spongeforge-1.16.5/config/westerosblocks/assets/westerosblocks/lang/*.json src/main/resources/assets/westerosblocks/lang
-cp ../../../Desktop/minecraft_servers/spongeforge-1.16.5/config/westerosblocks/assets/westerosblocks/blockstates/*.json src/main/resources/assets/westerosblocks/blockstates
-cp -R ../../../Desktop/minecraft_servers/spongeforge-1.16.5/config/westerosblocks/assets/westerosblocks/models/block/generated/. src/main/resources/assets/westerosblocks/models/block/generated
-cp ../../../Desktop/minecraft_servers/spongeforge-1.16.5/config/westerosblocks/assets/westerosblocks/models/item//*.json src/main/resources/assets/westerosblocks/models/item
-cp ../../../Desktop/minecraft_servers/spongeforge-1.16.5/config/westerosblocks/data/minecraft/tags/blocks/*.json src/main/resources/data/minecraft/tags/blocks
+cd /mnt/c/Users/Jacob/Desktop
+cp ./minecraft_servers/spongeforge-1.16.5/config/westerosblocks/assets/westerosblocks/lang/*.json ~/Working/WesterosBlocks/src/main/resources/assets/westerosblocks/lang
+cp ./minecraft_servers/spongeforge-1.16.5/config/westerosblocks/assets/westerosblocks/blockstates/*.json ~/Working/WesterosBlocks/src/main/resources/assets/westerosblocks/blockstates
+cp -R ./minecraft_servers/spongeforge-1.16.5/config/westerosblocks/assets/westerosblocks/models/block/generated/. ~/Working/WesterosBlocks/src/main/resources/assets/westerosblocks/models/block/generated
+cp ./minecraft_servers/spongeforge-1.16.5/config/westerosblocks/assets/westerosblocks/models/item//*.json ~/Working/WesterosBlocks/src/main/resources/assets/westerosblocks/models/item
+cp ./minecraft_servers/spongeforge-1.16.5/config/westerosblocks/data/minecraft/tags/blocks/*.json ~/Working/WesterosBlocks/src/main/resources/data/minecraft/tags/blocks
