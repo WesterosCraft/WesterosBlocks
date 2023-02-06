be2fe00c9aea7ce2932502e8a5456dcb9220f1b3
Mike Primm <mike@primmhome.com>
2023-02-05 23:09:05 -0600

    Add second betterfoliage model with offset

--- src/main/java/com/westeroscraft/westerosblocks/modelexport/LeavesBlockModelExport.java
+++ src/main/java/com/westeroscraft/westerosblocks/modelexport/LeavesBlockModelExport.java
@@ -64,6 +64,16 @@ public class LeavesBlockModelExport extends ModelExport {
             	if (i > 0) var.y = 90*i;
             	vars.add(var);
             }
+            if (blk.betterfoliage) {
+            	model = WesterosBlocks.MOD_ID + ":block/generated/" + getModelName("base2", setidx);
+                for (int i = 0; i < cnt; i++) {
+                	Variant var = new Variant();
+                	var.model = model;
+                	var.weight = set.weight;
+                	if (i > 0) var.y = 90*i;
+                	vars.add(var);
+                }            	
+            }
         }
         if (vars.size() == 1) {
             StateObject1 so = new StateObject1();
@@ -79,7 +89,7 @@ public class LeavesBlockModelExport extends ModelExport {
         }
     }
     
-    private ModelObjectLeaves makeModel(int setidx, boolean isTinted, boolean bf, boolean overlay) {
+    private ModelObjectLeaves makeModel(int setidx, boolean isTinted, boolean bf, boolean overlay, int bfidx) {
     	WesterosBlockDef.RandomTextureSet set = def.getRandomTextureSet(setidx);
     	
         ModelObjectLeaves mod = new ModelObjectLeaves();
@@ -90,11 +100,13 @@ public class LeavesBlockModelExport extends ModelExport {
     		mod.textures.bf = getTextureID(set.getTextureByIndex(2)); 
         	if (overlay) {
         		mod.parent = isTinted ? "westerosblocks:block/tinted/leaves_overlay_bf" : "westerosblocks:block/untinted/leaves_overlay_bf";
+        		if (bfidx > 0) mod.parent = mod.parent + (bfidx+1);
         		mod.textures.overlayend = getTextureID(set.getTextureByIndex(3)); 
         		mod.textures.overlayside = getTextureID(set.getTextureByIndex(4)); 
         	}
         	else {
         		mod.parent = isTinted ? "westerosblocks:block/tinted/leaves_bf" : "westerosblocks:block/untinted/leaves_bf";        		
+           		if (bfidx > 0) mod.parent = mod.parent + (bfidx+1);
         	}
         }
         else {
@@ -115,12 +127,19 @@ public class LeavesBlockModelExport extends ModelExport {
         
         // Loop over the random sets we've got
         for (int setidx = 0; setidx < def.getRandomTextureSetCount(); setidx++) {
-            ModelObjectLeaves mod = makeModel(setidx, isTinted, blk.betterfoliage, blk.overlay);
+            ModelObjectLeaves mod = makeModel(setidx, isTinted, blk.betterfoliage, blk.overlay, 0);
 
             this.writeBlockModelFile(getModelName("base", setidx), mod);
+            
+            if (blk.betterfoliage) {
+                mod = makeModel(setidx, isTinted, blk.betterfoliage, blk.overlay, 1);
+
+                this.writeBlockModelFile(getModelName("base2", setidx), mod);
+            	
+            }
         }
         // Build base model for item
-        ModelObjectLeaves mo = makeModel(0, isTinted, false, blk.overlay);
+        ModelObjectLeaves mo = makeModel(0, isTinted, false, blk.overlay, 0);
         this.writeItemModelFile(def.getBlockName(), mo);
         // Add tint overrides
         if (isTinted) {
