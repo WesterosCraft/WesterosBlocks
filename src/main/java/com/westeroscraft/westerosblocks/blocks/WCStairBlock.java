9bac25104add6f33fe0f467d0c6d6bcaa2f5f3c1
Mike Primm <mike@primmhome.com>
2023-02-20 11:01:06 -0600

    gold_stairs\

--- src/main/java/com/westeroscraft/westerosblocks/blocks/WCStairBlock.java
+++ src/main/java/com/westeroscraft/westerosblocks/blocks/WCStairBlock.java
@@ -26,60 +26,78 @@ public class WCStairBlock extends StairBlock implements WesterosBlockLifecycle {
             // Try to find model block
             Block blk = WesterosBlocks.findBlockByName(def.modelBlockName);
             if (blk == null) {
-                WesterosBlocks.log.error(String.format("modelBlockName '%s' not found for block '%s'", def.modelBlockName, def.blockName));
+                WesterosBlocks.log.error(String.format("modelBlockName '%s' not found for block '%s'",
+                        def.modelBlockName, def.blockName));
                 return null;
             }
             if (blk.defaultBlockState().isAir()) {
-                WesterosBlocks.log.error(String.format("modelBlockName '%s' not yet defined for block '%s' - must be defined before block", def.modelBlockName, def.blockName));            	
+                WesterosBlocks.log.error(String.format(
+                        "modelBlockName '%s' not yet defined for block '%s' - must be defined before block",
+                        def.modelBlockName, def.blockName));
                 return null;
             }
-        	BlockBehaviour.Properties props = def.makeAndCopyProperties(blk);
-        	return def.registerRenderType(def.registerBlock(new WCStairBlock(blk.defaultBlockState(), props, def)), false, false);
+            WesterosBlockDef mbdef = null;
+            // If a WB, look up def
+            if (blk instanceof WesterosBlockLifecycle) {
+                mbdef = ((WesterosBlockLifecycle) blk).getWBDefinition();
+                // See if we have a cond property
+                WesterosBlockDef.CondProperty prop = mbdef.buildCondProperty();
+                if (prop != null) {
+                    tempCOND = prop;
+                }
+            }
+            BlockBehaviour.Properties props = def.makeAndCopyProperties(blk);
+            return def.registerRenderType(def.registerBlock(new WCStairBlock(blk.defaultBlockState(), props, def)),
+                    false, false);
         }
     }
-    
+
     private WesterosBlockDef def;
     private static WesterosBlockDef.CondProperty tempCOND;
     private WesterosBlockDef.CondProperty COND;
-    
+
     protected WCStairBlock(BlockState modelstate, BlockBehaviour.Properties props, WesterosBlockDef def) {
         super(() -> modelstate, props);
         this.def = def;
         if (COND != null) {
-            this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH).setValue(HALF, Half.BOTTOM).setValue(SHAPE, StairsShape.STRAIGHT).setValue(WATERLOGGED, Boolean.valueOf(false)).setValue(COND, COND.defValue));
+            this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH)
+                    .setValue(HALF, Half.BOTTOM).setValue(SHAPE, StairsShape.STRAIGHT)
+                    .setValue(WATERLOGGED, Boolean.valueOf(false)).setValue(COND, COND.defValue));
         }
     }
+
     @Override
     public WesterosBlockDef getWBDefinition() {
         return def;
     }
-    
+
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> StateDefinition) {
-    	super.createBlockStateDefinition(StateDefinition);
-    	if (tempCOND != null) {
-    		COND = tempCOND;
-    		tempCOND = null;
-    	}
-    	if (COND != null) {
-	       StateDefinition.add(COND);
-    	}
+        super.createBlockStateDefinition(StateDefinition);
+        if (tempCOND != null) {
+            COND = tempCOND;
+            tempCOND = null;
+        }
+        if (COND != null) {
+            StateDefinition.add(COND);
+        }
     }
- 
+
     @Override
     @Nullable
     public BlockState getStateForPlacement(BlockPlaceContext ctx) {
-    	BlockState bs = super.getStateForPlacement(ctx);
-    	if ((bs != null) && (COND != null)) {
-    		bs = bs.setValue(COND, def.getMatchingCondition(ctx.getLevel(), ctx.getClickedPos())); 
-    	}
-    	return bs;
+        BlockState bs = super.getStateForPlacement(ctx);
+        if ((bs != null) && (COND != null)) {
+            bs = bs.setValue(COND, def.getMatchingCondition(ctx.getLevel(), ctx.getClickedPos()));
+        }
+        return bs;
     }
 
     private static String[] TAGS = { "stairs" };
+
     @Override
     public String[] getBlockTags() {
-    	return TAGS;
-    }    
+        return TAGS;
+    }
 
 }
