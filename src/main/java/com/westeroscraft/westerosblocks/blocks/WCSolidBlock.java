5bba4165cb506e4656aa057acdc7965e70c5803e
Mike Primm <mike@primmhome.com>
2023-02-20 23:49:09 -0600

    Normalize names

--- src/main/java/com/westeroscraft/westerosblocks/blocks/WCSolidBlock.java
+++ src/main/java/com/westeroscraft/westerosblocks/blocks/WCSolidBlock.java
@@ -64,9 +64,9 @@ public class WCSolidBlock extends Block implements WesterosBlockLifecycle {
     		return collisionbox;
     }
     
-    public boolean skipRendering(BlockState p_53972_, BlockState p_53973_, Direction p_53974_) {
+    public boolean skipRendering(BlockState state, BlockState other_state, Direction direction) {
     	if (def.nonOpaque)
-    		return p_53973_.is(this) ? true : super.skipRendering(p_53972_, p_53973_, p_53974_);
+    		return other_state.is(this) ? true : super.skipRendering(state, other_state, direction);
     	else
     		return false;
     }
