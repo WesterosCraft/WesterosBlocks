2eb5358a4e8651fd243e82e88293a237f052a241
Mike Primm <mike@primmhome.com>
2023-08-14 22:51:05 -0500

    Switch custom doors to allow_unsupported

--- src/main/java/com/westeroscraft/westerosblocks/blocks/WCDoorBlock.java
+++ src/main/java/com/westeroscraft/westerosblocks/blocks/WCDoorBlock.java
@@ -2,14 +2,17 @@ package com.westeroscraft.westerosblocks.blocks;
 
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.DoubleBlockHalf;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.level.block.DoorBlock;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 
 import com.westeroscraft.westerosblocks.WesterosBlockDef;
 import com.westeroscraft.westerosblocks.WesterosBlockLifecycle;
@@ -28,6 +31,7 @@ public class WCDoorBlock extends DoorBlock implements WesterosBlockLifecycle {
     
     private WesterosBlockDef def;
     private boolean locked = false;
+    private boolean allow_unsupported = false;
 
     protected WCDoorBlock(BlockBehaviour.Properties props, WesterosBlockDef def) {
         super(props);
@@ -36,7 +40,10 @@ public class WCDoorBlock extends DoorBlock implements WesterosBlockLifecycle {
         if (type != null) {
             String[] toks = type.split(",");
             for (String tok : toks) {
-                String [] flds = tok.split(":");
+            	if (tok.equals("allow-unsupported")) {
+                    allow_unsupported = true;
+                }
+            	String [] flds = tok.split(":");
                 if (flds.length < 2) continue;
                 if (flds[0].equals("locked")) {
                     locked = flds[1].equals("true");
@@ -57,6 +64,11 @@ public class WCDoorBlock extends DoorBlock implements WesterosBlockLifecycle {
         	return super.use(p_225533_1_, p_225533_2_, p_225533_3_, p_225533_4_, p_225533_5_, p_225533_6_);
         }
     }
+    @Override
+    public boolean canSurvive(BlockState p_52783_, LevelReader p_52784_, BlockPos p_52785_) {
+    	if (allow_unsupported) return true;
+    	return super.canSurvive(p_52783_, p_52784_, p_52785_);
+     }
 
     private static String[] TAGS = { "doors" };
     @Override
