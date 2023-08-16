e3f4d08ad435603bf83ddc89656bbbfa32b3dd90
Mike Primm <mike@primmhome.com>
2023-08-15 23:40:48 -0500

    Add allow-unsupported to rails, switch blocks to use it

--- src/main/java/com/westeroscraft/westerosblocks/blocks/WCRailBlock.java
+++ src/main/java/com/westeroscraft/westerosblocks/blocks/WCRailBlock.java
@@ -1,6 +1,9 @@
 package com.westeroscraft.westerosblocks.blocks;
 
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.RailBlock;
 
@@ -19,16 +22,33 @@ public class WCRailBlock extends RailBlock implements WesterosBlockLifecycle {
     }
     
     private WesterosBlockDef def;
+    private boolean allow_unsupported = false;
     
     protected WCRailBlock(BlockBehaviour.Properties props, WesterosBlockDef def) {
         super(props);
         this.def = def;
+        String t = def.getType();
+        if (t != null) {
+            String[] toks = t.split(",");
+            for (String tok : toks) {
+                if (tok.equals("allow-unsupported")) {
+                    allow_unsupported = true;
+                }
+            }
+        }
+
     }
 
     @Override
     public WesterosBlockDef getWBDefinition() {
         return def;
     }    
+
+    @Override
+    public boolean canSurvive(BlockState p_49395_, LevelReader p_49396_, BlockPos p_49397_) {
+    	if (this.allow_unsupported) return true;
+        return super.canSurvive(p_49395_, p_49396_, p_49397_);
+     }
 
     private static String[] TAGS = { "rails" };
     @Override
