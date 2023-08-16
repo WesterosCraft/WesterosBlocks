976bc0844cc98e237d06d0314764191f77ecfe93
Mike Primm <mike@primmhome.com>
2023-08-16 11:56:45 -0500

    Add neighborChanged fix for rails with allow-unsupported

--- src/main/java/com/westeroscraft/westerosblocks/blocks/WCRailBlock.java
+++ src/main/java/com/westeroscraft/westerosblocks/blocks/WCRailBlock.java
@@ -2,7 +2,9 @@ package com.westeroscraft.westerosblocks.blocks;
 
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.RailShape;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.RailBlock;
@@ -49,6 +51,16 @@ public class WCRailBlock extends RailBlock implements WesterosBlockLifecycle {
     	if (this.allow_unsupported) return true;
         return super.canSurvive(p_49395_, p_49396_, p_49397_);
      }
+
+    @Override
+    public void neighborChanged(BlockState p_49377_, Level p_49378_, BlockPos p_49379_, Block p_49380_, BlockPos p_49381_, boolean p_49382_) {
+    	if (!this.allow_unsupported) { 
+    		super.neighborChanged(p_49377_, p_49378_, p_49379_, p_49380_, p_49381_, p_49382_);
+    	}
+    	else if (!p_49378_.isClientSide && p_49378_.getBlockState(p_49379_).is(this)) {
+        	this.updateState(p_49377_, p_49378_, p_49379_, p_49380_);
+        }
+    }
 
     private static String[] TAGS = { "rails" };
     @Override
