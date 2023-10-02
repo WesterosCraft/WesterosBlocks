60a76e9ac41ccba3cdfab7ea04153d9cc84df50b
Mike Primm <mike@primmhome.com>
2023-10-01 20:13:53 -0500

    Remove grass_layer

--- src/main/java/com/westeroscraft/westerosblocks/WesterosBlocks.java
+++ src/main/java/com/westeroscraft/westerosblocks/WesterosBlocks.java
@@ -479,7 +479,7 @@ public class WesterosBlocks {
 	// Network setup
 	public static SimpleChannel simpleChannel;    // used to transmit your network messages
     public static final String CHANNEL = "wbchannel";
-    public static final String MESSAGE_PROTOCOL_VERSION = "5.4";
+    public static final String MESSAGE_PROTOCOL_VERSION = "5.5";
     public static final ResourceLocation simpleChannelRL = new ResourceLocation("westerosblocks", CHANNEL);
 
     @SubscribeEvent
