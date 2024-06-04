76c1373beb83c69c58daddb07cfbbf505f28375a
Jacob <jacobgranberry@gmail.com>
2024-06-04 14:38:50 -0500

    more tinkering

--- src/main/java/com/westeroscraft/westerosblocks/blocks/WCParticleBlock.java
+++ src/main/java/com/westeroscraft/westerosblocks/blocks/WCParticleBlock.java
@@ -4,8 +4,10 @@ import com.westeroscraft.westerosblocks.WesterosBlockDef;
 import com.westeroscraft.westerosblocks.WesterosBlockFactory;
 import com.westeroscraft.westerosblocks.WesterosBlockLifecycle;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SimpleWaterloggedBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
@@ -17,17 +19,21 @@ import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.NotNull;
 
 public class WCParticleBlock extends Block implements SimpleWaterloggedBlock, WesterosBlockLifecycle {
     private final WesterosBlockDef def;
-    protected static final VoxelShape BLOCK_SHAPE = Block.box(4.0D, 4.0D, 4.0D, 12.0D, 12.0D, 12.0D);
+    protected static final VoxelShape OFF_SHAPE = Block.box(4.0D, 4.0D, 4.0D, 12.0D, 12.0D, 12.0D);
+    protected static final VoxelShape ON_SHAPE = Block.box(6.0D, 6.0D, 6.0D, 10.0D, 10.0D, 10.0D);
+
     public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
     public static final BooleanProperty POWERED = BlockStateProperties.POWERED;
+    public static final IntegerProperty PARTICLE_RANGE = IntegerProperty.create("range", 1, 100);
     public static final IntegerProperty PARTICLE_TYPE = IntegerProperty.create("type", 1, 100);
     public static final IntegerProperty PARTICLE_STRENGTH = IntegerProperty.create("strength", 1, 100);
-    public static final IntegerProperty PARTICLE_RANGE = IntegerProperty.create("range", 1, 100);
+
 
     public static class Factory extends WesterosBlockFactory {
         @Override
@@ -50,13 +56,21 @@ public class WCParticleBlock extends Block implements SimpleWaterloggedBlock, We
 
     @Override
     public VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
-            return BLOCK_SHAPE;
+        if (state.getValue(POWERED)) {
+            return ON_SHAPE;
+        } else {
+            return OFF_SHAPE;
+        }
     }
 
     @Override
     public VoxelShape getCollisionShape(BlockState state, @NotNull BlockGetter p_58056_, BlockPos p_58057_,
                                         CollisionContext p_58058_) {
-        return BLOCK_SHAPE;
+        if (state.getValue(POWERED)) {
+            return Shapes.empty();
+        } else {
+            return OFF_SHAPE;
+        }
     }
 
     @Override
@@ -68,6 +82,16 @@ public class WCParticleBlock extends Block implements SimpleWaterloggedBlock, We
         FluidState fluidstate = context.getLevel().getFluidState(context.getClickedPos());
 
         return this.defaultBlockState().setValue(POWERED, false).setValue(PARTICLE_TYPE, 1).setValue(PARTICLE_STRENGTH, 1).setValue(PARTICLE_RANGE, 1).setValue(WATERLOGGED, Boolean.valueOf(fluidstate.getType() == Fluids.WATER));
+    }
+
+    @Override
+    public BlockState updateShape(BlockState state, Direction dir, BlockState state2, LevelAccessor world, BlockPos pos,
+                                  BlockPos pos2) {
+        if (state.getValue(WATERLOGGED)) {
+            world.scheduleTick(pos, Fluids.WATER, Fluids.WATER.getTickDelay(world));
+        }
+
+        return super.updateShape(state, dir, state2, world, pos, pos2);
     }
 
     private static final String[] TAGS = { "particles" };
