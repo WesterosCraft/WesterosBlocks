67031457cafc5c3486a824451b3b8f6af2bb21d3
emoticone11 <wc.emoticone11@gmail.com>
2024-12-21 15:08:19 -0500

    Updated texture stitching script

--- src/main/resources/scripts/stitch_textures.py
+++ src/main/resources/scripts/stitch_textures.py
@@ -1,6 +1,7 @@
 import os
 import argparse
 import numpy as np
+import json
 import cv2
 
 EXTS = {'png', 'jpg', 'jpeg'}
@@ -9,12 +10,14 @@ WORKING_DIR = os.getcwd()
 SCRIPTS_DIR = 'scripts'
 ASSETS_DIR = 'assets'
 TEXTURES_DIR = 'textures'
+OUTPUT_DIR = os.path.join(CALLING_DIR, 'stitch')
+
+WB_FNAME = 'WesterosBlocks.json'
 
 ROOTS_CONFIG_FNAME = os.path.join(CALLING_DIR, 'roots.txt')
 BLACKLIST_CONFIG_FNAME = os.path.join(CALLING_DIR, 'blacklist.txt')
-STITCH_FNAME = os.path.join(CALLING_DIR, 'stitch.png')
 
-textures = []
+TEXTURES = []
 
 
 def trim(stitch):
@@ -32,7 +35,7 @@ def trim(stitch):
     return stitch
 
 
-def pack(W):
+def pack(W, textures):
   I = textures.copy()
   H_bound = sum([t.shape[1] for t in I])
   total_height = 0
@@ -56,7 +59,7 @@ def register(path):
   if texture.shape[2] == 3:
     texture = cv2.cvtColor(texture, cv2.COLOR_RGB2RGBA)
     texture[:,:,3] = np.ones((texture.shape[0], texture.shape[1]))*255
-  textures.append(texture)
+  TEXTURES.append(texture)
 
 
 def traverse(rootdir, blacklist):
@@ -68,10 +71,7 @@ def traverse(rootdir, blacklist):
       register(path)
 
 
-def main(roots, blacklist):
-  for root in roots:
-    traverse(root+'/'+TEXTURES_DIR, blacklist)
-
+def create_stitch(textures):
   textures.sort(key=lambda t:t.shape[0])
 
   W_max = max([t.shape[1] for t in textures])
@@ -79,19 +79,30 @@ def main(roots, blacklist):
 
   min_stitch = None
   for W in np.linspace(W_max*2, W_bound, 50, dtype=int):
-    ret = pack(W)
+    ret = pack(W, textures)
     if min_stitch is None or ret.shape[0]*ret.shape[1] < min_stitch.shape[0]*min_stitch.shape[1]:
       min_stitch = ret
-  print(min_stitch.shape)
+  print(f'Stitched {len(textures)}: {min_stitch.shape}')
+  return min_stitch
+
+
+def main(roots, blacklist, disaggregate=False, noctm=False):
+  for root in roots:
+    traverse(root+'/'+TEXTURES_DIR, blacklist)
+
+  textures_all = TEXTURES.copy()
+  min_stitch = create_stitch(textures_all)
 
   os.chdir(WORKING_DIR)
-  cv2.imwrite(STITCH_FNAME, min_stitch)
+  cv2.imwrite(OUTPUT_DIR+'/atlas.png', min_stitch)
 
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
   parser.add_argument('-r', '--roots', nargs='+', default=[])
   parser.add_argument('-b', '--blacklist', nargs='+', default=[])
+  parser.add_argument('-d', '--disaggregate', action='store_true')
+  parser.add_argument('-n', '--noctm', action='store_true')
 
   roots = []
   if os.path.exists(ROOTS_CONFIG_FNAME):
@@ -107,9 +118,11 @@ if __name__ == '__main__':
   args = parser.parse_args()
   roots = roots.union(set(args.roots))
   blacklist = blacklist.union(set(args.blacklist))
+  disaggregate = args.disaggregate
+  noctm = args.noctm
 
   os.chdir(os.path.abspath('/'.join(CALLING_DIR.split('/')[:-1])+'/'+ASSETS_DIR))
 
-  main(roots, blacklist)
+  main(roots, blacklist, disaggregate=disaggregate, noctm=noctm)
   
 
