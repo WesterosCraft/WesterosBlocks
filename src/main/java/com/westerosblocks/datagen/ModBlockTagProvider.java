7daa039f6d64c7b532c7f3093400a80b4ed308b5
Jacob <jacobgranberry@gmail.com>
2025-01-01 10:39:11 -0700

    fix blocktag datagen namespace issue

--- src/main/java/com/westerosblocks/datagen/ModBlockTagProvider.java
+++ src/main/java/com/westerosblocks/datagen/ModBlockTagProvider.java
@@ -2,11 +2,11 @@ package com.westerosblocks.datagen;
 
 import com.westerosblocks.WesterosBlocks;
 import com.westerosblocks.WesterosBlocksJsonLoader;
-import com.westerosblocks.block.ModBlocks;
 import com.westerosblocks.block.WesterosBlockTags;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.minecraft.block.Block;
+import net.minecraft.registry.Registries;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.registry.tag.TagKey;
@@ -25,10 +25,13 @@ public class ModBlockTagProvider extends FabricTagProvider.BlockTagProvider {
 
         if (config.blockTags != null) {
             for (WesterosBlockTags tag : config.blockTags) {
-                TagKey<Block> CUSTOM_TAG_NAME = TagKey.of(RegistryKeys.BLOCK, Identifier.of(WesterosBlocks.MOD_ID, tag.customTag));
-
                 for (String blockName : tag.blockNames) {
-                    getOrCreateTagBuilder(CUSTOM_TAG_NAME).add(ModBlocks.findBlockByName(blockName.split(":")[1], blockName.split(":")[0]));
+                    String namespace = blockName.split(":")[0];
+                    TagKey<Block> CUSTOM_TAG_NAME = TagKey.of(RegistryKeys.BLOCK,
+                            Identifier.of(namespace, tag.customTag));
+
+                    getOrCreateTagBuilder(CUSTOM_TAG_NAME)
+                            .add(Registries.BLOCK.get(Identifier.of(namespace, blockName.split(":")[1])));
                 }
             }
         }
