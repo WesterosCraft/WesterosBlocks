f70d036b9643370fc231afe660b6ca14fc6c144e
jacobgranberry <jacobgranberry@gmail.com>
2025-05-13 21:02:49 -0600

    shields, update fabric loader version

--- src/main/java/com/westerosblocks/item/ModItems.java
+++ src/main/java/com/westerosblocks/item/ModItems.java
@@ -18,11 +18,24 @@ public class ModItems {
     // TODO make the settings make sense
     public static final Item STARK_KITE_SHIELD = registerItem("stark_kite_shield",
             new KiteShieldItem(new Item.Settings().maxDamage(2800), 8, 15, Items.NETHERITE_INGOT, "stark_kite_shield"));
+
     public static final Item TULLY_HEATER_SHIELD = registerItem("tully_heater_shield",
             new HeaterShieldItem(new Item.Settings().maxDamage(2800), 3, 15, Items.NETHERITE_INGOT, "tully_heater_shield"));
+
     public static final Item GREYJOY_ROUND_SHIELD = registerItem("greyjoy_round_shield",
             new RoundShieldItem(new Item.Settings().maxDamage(2800), 3, 15, Items.NETHERITE_INGOT, "greyjoy_round_shield"));
 
+    public static final Item TARGARYEN_HEATER_SHIELD = registerItem("targaryen_heater_shield",
+            new HeaterShieldItem(new Item.Settings().maxDamage(2800), 3, 15, Items.NETHERITE_INGOT, "targaryen_heater_shield"));
+
+    public static final Item BLACKFYRE_HEATER_SHIELD = registerItem("blackfyre_heater_shield",
+            new HeaterShieldItem(new Item.Settings().maxDamage(2800), 3, 15, Items.NETHERITE_INGOT, "blackfyre_heater_shield"));
+
+    public static final Item BLACKWOOD_HEATER_SHIELD = registerItem("blackwood_heater_shield",
+            new HeaterShieldItem(new Item.Settings().maxDamage(2800), 3, 15, Items.NETHERITE_INGOT, "blackwood_heater_shield"));
+
+    public static final Item BRACKEN_HEATER_SHIELD = registerItem("bracken_heater_shield",
+            new HeaterShieldItem(new Item.Settings().maxDamage(2800), 3, 15, Items.NETHERITE_INGOT, "bracken_heater_shield"));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, WesterosBlocks.id(name), item);
@@ -38,6 +51,10 @@ public class ModItems {
             entries.add(STARK_KITE_SHIELD);
             entries.add(TULLY_HEATER_SHIELD);
             entries.add(GREYJOY_ROUND_SHIELD);
+            entries.add(BLACKFYRE_HEATER_SHIELD);
+            entries.add(BLACKWOOD_HEATER_SHIELD);
+            entries.add(BRACKEN_HEATER_SHIELD);
+            entries.add(TARGARYEN_HEATER_SHIELD);
         });
     }
 }
