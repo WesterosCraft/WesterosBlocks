31a627bdb705915bfbd6f312944d1fa1b884de70
jacob.granberry <jacob.granberry@company-corp.com>
2025-06-15 09:36:27 -0600

    Refactor WCArrowSlitBlock to implement ModBlockLifecycle, introduce ArrowSlitType for state management, and add blockstate JSON for rendering variants.

--- src/main/java/com/westerosblocks/block/custom/WCArrowSlitBlock.java
+++ src/main/java/com/westerosblocks/block/custom/WCArrowSlitBlock.java
@@ -1,10 +1,12 @@
 package com.westerosblocks.block.custom;
 
-import com.westerosblocks.block.*;
+import com.westerosblocks.block.ModBlocks;
+import com.westerosblocks.block.ModBlock;
+import com.westerosblocks.block.ModBlockFactory;
+import com.westerosblocks.block.ModBlockLifecycle;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.fluid.FluidState;
@@ -14,123 +16,98 @@ import net.minecraft.registry.tag.FluidTags;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.state.property.DirectionProperty;
+import net.minecraft.state.property.EnumProperty;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldAccess;
 import net.minecraft.world.WorldView;
+import net.minecraft.util.StringIdentifiable;
 
-import java.util.ArrayList;
-
-public class WCArrowSlitBlock extends WCCuboidBlock {
+public class WCArrowSlitBlock extends Block implements ModBlockLifecycle {
 
     public static class Factory extends ModBlockFactory {
         @Override
         public Block buildBlockClass(ModBlock def) {
-            def.nonOpaque = true;
             AbstractBlock.Settings settings = def.applyCustomProperties();
             Block blk = new WCArrowSlitBlock(settings, def);
             return def.registerRenderType(ModBlocks.registerBlock(def.blockName, blk), false, false);
         }
     }
 
-    // Support waterlogged on these blocks
-    public static final BooleanProperty WATERLOGGED = Properties.WATERLOGGED;
-    public static final DirectionProperty FACING = DirectionProperty.of("facing", Direction.EAST, Direction.SOUTH, Direction.WEST, Direction.NORTH);
-    public static final BooleanProperty STACKED = BooleanProperty.of("stacked");
+    private final ModBlock def;
+    public static final EnumProperty<ArrowSlitType> TYPE = EnumProperty.of("type", ArrowSlitType.class);
 
     protected WCArrowSlitBlock(AbstractBlock.Settings settings, ModBlock def) {
-        super(settings, def, 8); // 8 possible states: 4 directions * 2 stacked states
-        
-        // Initialize cuboids for each facing direction and stacked state
-        for (int i = 0; i < 2; i++) { // i=0 for unstacked, i=1 for stacked
-            ModBlockStateRecord se = def.getStackElementByIndex(i);
-            for (ModBlock.Cuboid c : se.cuboids) {
-                cuboid_by_facing[4*i] = new ArrayList<>(); // East facing
-                cuboid_by_facing[4*i].add(c);
-                cuboid_by_facing[4*i + 1] = new ArrayList<>(); // South facing
-                cuboid_by_facing[4*i + 1].add(c.rotateCuboid(ModBlock.CuboidRotation.ROTY90));
-                cuboid_by_facing[4*i + 2] = new ArrayList<>(); // West facing
-                cuboid_by_facing[4*i + 2].add(c.rotateCuboid(ModBlock.CuboidRotation.ROTY180));
-                cuboid_by_facing[4*i + 3] = new ArrayList<>(); // North facing
-                cuboid_by_facing[4*i + 3].add(c.rotateCuboid(ModBlock.CuboidRotation.ROTY270));
-            }
-        }
-
-        // Create shapes for all combinations
-        for (int j = 0; j < cuboid_by_facing.length; j++) {
-            SHAPE_BY_INDEX[j] = getBoundingBoxFromCuboidList(cuboid_by_facing[j]);
-        }
-
-        this.setDefaultState(this.getDefaultState()
-                .with(FACING, Direction.EAST)
-                .with(STACKED, false)
-                .with(WATERLOGGED, false));
+        super(settings);
+        this.def = def;
+        setDefaultState(getDefaultState().with(TYPE, ArrowSlitType.SINGLE));
     }
 
     @Override
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-        super.appendProperties(builder);
-        builder.add(FACING, STACKED);
+        builder.add(TYPE);
     }
 
     @Override
-    protected int getIndexFromState(BlockState state) {
-        int stackedOffset = state.get(STACKED) ? 4 : 0;
-        return switch (state.get(FACING)) {
-            case SOUTH -> stackedOffset + 1;
-            case WEST -> stackedOffset + 2;
-            case NORTH -> stackedOffset + 3;
-            default -> stackedOffset;
-        };
-    }
-
-    @Override
-    public BlockState getPlacementState(ItemPlacementContext ctx) {
-        BlockPos blockpos = ctx.getBlockPos();
-        FluidState fluidstate = ctx.getWorld().getFluidState(blockpos);
-        Direction[] adirection = ctx.getPlacementDirections();
-        Direction dir = Direction.EAST; // Default
-        for (Direction d : adirection) {
-            if (d == Direction.EAST || d == Direction.WEST || d == Direction.NORTH || d == Direction.SOUTH) {
-                dir = d;
-                break;
+    public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
+        if (direction == Direction.UP || direction == Direction.DOWN) {
+            boolean hasTop = world.getBlockState(pos.up()).isOf(this);
+            boolean hasBottom = world.getBlockState(pos.down()).isOf(this);
+            
+            ArrowSlitType newType;
+            if (hasTop && hasBottom) {
+                newType = ArrowSlitType.MID;
+            } else if (hasTop) {
+                newType = ArrowSlitType.BOTTOM;
+            } else if (hasBottom) {
+                newType = ArrowSlitType.TOP;
+            } else {
+                newType = ArrowSlitType.SINGLE;
             }
+            
+            return state.with(TYPE, newType);
         }
-        return this.getDefaultState()
-                .with(FACING, dir)
-                .with(WATERLOGGED, fluidstate.isIn(FluidTags.WATER));
-    }
-
-    @Override
-    public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState,
-                                              WorldAccess world, BlockPos pos, BlockPos neighborPos) {
-        if (direction == Direction.UP) {
-            boolean isStacked = neighborState.isOf(this);
-            return state.with(STACKED, isStacked);
-        }
-        return super.getStateForNeighborUpdate(state, direction, neighborState, world, pos, neighborPos);
+        return state;
     }
 
     @Override
     public void onPlaced(World world, BlockPos pos, BlockState state, LivingEntity placer, ItemStack itemStack) {
-        // Update the block below if it exists
-        BlockPos below = pos.down();
-        BlockState belowState = world.getBlockState(below);
-        if (belowState.isOf(this)) {
-            world.setBlockState(below, belowState.with(STACKED, true), 3);
-        }
+        // The getStateForNeighborUpdate method will handle state updates for neighboring blocks
     }
 
     @Override
     public BlockState onBreak(World world, BlockPos pos, BlockState state, PlayerEntity player) {
-        // Update the block below if it exists
-        BlockPos below = pos.down();
-        BlockState belowState = world.getBlockState(below);
-        if (belowState.isOf(this)) {
-            world.setBlockState(below, belowState.with(STACKED, false), 3);
-        }
+        // The getStateForNeighborUpdate method will handle state updates for neighboring blocks
         return super.onBreak(world, pos, state, player);
+    }
+
+    @Override
+    public ModBlock getWBDefinition() {
+        return def;
+    }
+
+    @Override
+    public String[] getBlockTags() {
+        return new String[0];
+    }
+
+    public enum ArrowSlitType implements StringIdentifiable {
+        SINGLE("single"),
+        TOP("top"),
+        BOTTOM("bottom"),
+        MID("mid");
+
+        private final String name;
+
+        ArrowSlitType(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String asString() {
+            return this.name;
+        }
     }
 } 
