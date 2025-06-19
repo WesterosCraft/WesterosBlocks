ca40c039b69ce9f1a88aa53f5778d3f3d838b606
jacobgranberry <jacobgranberry@gmail.com>
2025-06-18 21:35:46 -0600

    add missing textures, fix warnings

--- src/main/java/com/westerosblocks/datagen/models/TableBlockExport.java
+++ src/main/java/com/westerosblocks/datagen/models/TableBlockExport.java
@@ -113,17 +113,21 @@ public class TableBlockExport {
                 .put(VariantSettings.MODEL, centerModelId))
 
             .register(false, true, true, true, false, BlockStateVariant.create()
-                .put(VariantSettings.MODEL, centerModelId))
+                .put(VariantSettings.MODEL, centerModelId)
+                .put(VariantSettings.Y, VariantSettings.Rotation.R90))
             .register(false, true, true, true, true, BlockStateVariant.create()
-                .put(VariantSettings.MODEL, centerModelId))
+                .put(VariantSettings.MODEL, centerModelId)
+                .put(VariantSettings.Y, VariantSettings.Rotation.R90))
             .register(true, false, true, true, false, BlockStateVariant.create()
                 .put(VariantSettings.MODEL, centerModelId))
             .register(true, false, true, true, true, BlockStateVariant.create()
                 .put(VariantSettings.MODEL, centerModelId))
             .register(true, true, false, true, false, BlockStateVariant.create()
-                .put(VariantSettings.MODEL, centerModelId))
+                .put(VariantSettings.MODEL, centerModelId)
+                .put(VariantSettings.Y, VariantSettings.Rotation.R270))
             .register(true, true, false, true, true, BlockStateVariant.create()
-                .put(VariantSettings.MODEL, centerModelId))
+                .put(VariantSettings.MODEL, centerModelId)
+                .put(VariantSettings.Y, VariantSettings.Rotation.R270))
             .register(true, true, true, false, false, BlockStateVariant.create()
                 .put(VariantSettings.MODEL, centerModelId))
             .register(true, true, true, false, true, BlockStateVariant.create()
@@ -152,23 +156,29 @@ public class TableBlockExport {
 
         JsonArray elements = new JsonArray();
         
-        elements.add(createTableTop());
-
         switch (type) {
             case SINGLE -> {
-                elements.add(createTableLeg(2, 0, 2));
-                elements.add(createTableLeg(2, 0, 10));
-                elements.add(createTableLeg(10, 0, 10));
-                elements.add(createTableLeg(10, 0, 2));
+                elements.add(createTableTopSingle());
+                elements.add(createTableTopInnerSingle());
+                elements.add(createTableLegSingle(1, 0, 1, 13, 5, 16, 16));
+                elements.add(createTableLegSingle(1, 0, 12, 0, 5, 3, 16));
+                elements.add(createTableLegSingle(12, 0, 12, 13, 5, 16, 16));
+                elements.add(createTableLegSingle(12, 0, 1, 0, 5, 3, 16));
             }
             case DOUBLE -> {
-                elements.add(createTableLeg(2, 0, 2));
-                elements.add(createTableLeg(10, 0, 2));
+                elements.add(createTableLegDouble(1, 0, 1, 0, 5, 3, 16));
+                elements.add(createTableLegDouble(12, 0, 1, 13, 5, 16, 16));
+                elements.add(createTableTopDouble());
+                elements.add(createTableTopInnerDouble());
             }
             case CENTER -> {
+                elements.add(createTableTopCenter());
+                elements.add(createTableTopInnerCenter());
             }
             case CORNER -> {
-                elements.add(createTableLeg(2, 0, 2));
+                elements.add(createTableTopCorner());
+                elements.add(createTableTopInnerCorner());
+                elements.add(createTableLegCorner());
             }
         }
 
@@ -182,37 +192,203 @@ public class TableBlockExport {
         return modelId;
     }
 
-    private static JsonObject createTableTop() {
+    private static JsonObject createTableTopSingle() {
         JsonObject element = new JsonObject();
         element.addProperty("name", "top");
-        element.add("from", createArray(0, 12, 0));
+        element.add("from", createArray(0, 14, 0));
         element.add("to", createArray(16, 16, 16));
+        element.add("rotation", createRotation(0, "y", 8, 10.25, 8));
 
         JsonObject faces = new JsonObject();
-        faces.add("north", createFace(0, 0, 16, 4, "#1"));
-        faces.add("east", createFace(0, 0, 16, 4, "#1"));
-        faces.add("south", createFace(0, 0, 16, 4, "#1"));
-        faces.add("west", createFace(0, 0, 16, 4, "#1"));
-        faces.add("up", createFace(0, 0, 16, 16, "#1"));
+        faces.add("north", createFaceWithCullface(0, 0, 16, 2, "#1", "north"));
+        faces.add("east", createFaceWithCullface(0, 0, 16, 2, "#1", "east"));
+        faces.add("south", createFaceWithCullface(0, 0, 16, 2, "#1", "south"));
+        faces.add("west", createFaceWithCullface(0, 0, 16, 2, "#1", "west"));
+        faces.add("up", createFaceWithCullface(0, 0, 16, 16, "#1", "up"));
         faces.add("down", createFace(0, 0, 16, 16, "#1"));
         element.add("faces", faces);
 
         return element;
     }
 
-    private static JsonObject createTableLeg(int x, int y, int z) {
+    private static JsonObject createTableTopInnerSingle() {
+        JsonObject element = new JsonObject();
+        element.addProperty("name", "top");
+        element.add("from", createArray(1, 11, 1));
+        element.add("to", createArray(15, 14, 15));
+        element.add("rotation", createRotation(0, "y", 8, 10.25, 8));
+
+        JsonObject faces = new JsonObject();
+        faces.add("north", createFace(0, 2, 16, 5, "#1"));
+        faces.add("east", createFace(0, 2, 16, 5, "#1"));
+        faces.add("south", createFace(0, 2, 16, 5, "#1"));
+        faces.add("west", createFace(0, 2, 16, 5, "#1"));
+        faces.add("down", createFace(1, 1, 15, 15, "#1"));
+        element.add("faces", faces);
+
+        return element;
+    }
+
+    private static JsonObject createTableLegSingle(int x, int y, int z, int u1, int v1, int u2, int v2) {
         JsonObject element = new JsonObject();
         element.addProperty("name", "leg");
         element.add("from", createArray(x, y, z));
-        element.add("to", createArray(x + 4, y + 12, z + 4));
+        element.add("to", createArray(x + 3, y + 11, z + 3));
+        element.add("rotation", createRotation(0, "y", 8, 10.25, 8));
 
         JsonObject faces = new JsonObject();
-        faces.add("north", createFace(0, 0, 4, 16, "#1"));
-        faces.add("east", createFace(0, 0, 4, 16, "#1"));
-        faces.add("south", createFace(0, 0, 4, 16, "#1"));
-        faces.add("west", createFace(0, 0, 4, 16, "#1"));
-        faces.add("up", createFace(0, 0, 2, 2, "#missing"));
-        faces.add("down", createFace(0, 0, 4, 4, "#1"));
+        faces.add("north", createFace(u1, v1, u2, v2, "#1"));
+        faces.add("east", createFace(u1, v1, u2, v2, "#1"));
+        faces.add("south", createFace(u1, v1, u2, v2, "#1"));
+        faces.add("west", createFace(u1, v1, u2, v2, "#1"));
+        faces.add("down", createFaceWithCullface(u1, 13, u2, 16, "#1", "down"));
+        element.add("faces", faces);
+
+        return element;
+    }
+
+    private static JsonObject createTableLegDouble(int x, int y, int z, int u1, int v1, int u2, int v2) {
+        JsonObject element = new JsonObject();
+        element.addProperty("name", "leg");
+        element.add("from", createArray(x, y, z));
+        element.add("to", createArray(x + 3, y + 11, z + 3));
+        element.add("rotation", createRotation(0, "y", 8, 10.25, 5.5));
+
+        JsonObject faces = new JsonObject();
+        faces.add("north", createFace(u1, v1, u2, v2, "#1"));
+        faces.add("east", createFace(u1, v1, u2, v2, "#1"));
+        faces.add("south", createFace(u1, v1, u2, v2, "#1"));
+        faces.add("west", createFace(u1, v1, u2, v2, "#1"));
+        faces.add("down", createFaceWithCullface(u1, 13, u2, 16, "#1", "down"));
+        element.add("faces", faces);
+
+        return element;
+    }
+
+    private static JsonObject createTableTopDouble() {
+        JsonObject element = new JsonObject();
+        element.addProperty("name", "top");
+        element.add("from", createArray(0, 14, 0));
+        element.add("to", createArray(16, 16, 16));
+        element.add("rotation", createRotation(0, "y", 8, 10.25, 5.5));
+
+        JsonObject faces = new JsonObject();
+        faces.add("north", createFaceWithCullface(0, 0, 16, 2, "#1", "north"));
+        faces.add("east", createFaceWithCullface(0, 0, 16, 2, "#1", "east"));
+        faces.add("south", createFaceWithCullface(0, 0, 16, 2, "#1", "south"));
+        faces.add("west", createFaceWithCullface(0, 0, 16, 2, "#1", "west"));
+        faces.add("up", createFaceWithCullface(0, 0, 16, 16, "#1", "up"));
+        faces.add("down", createFace(0, 0, 16, 16, "#1"));
+        element.add("faces", faces);
+
+        return element;
+    }
+
+    private static JsonObject createTableTopInnerDouble() {
+        JsonObject element = new JsonObject();
+        element.addProperty("name", "top");
+        element.add("from", createArray(1, 11, 1));
+        element.add("to", createArray(15, 14, 16));
+        element.add("rotation", createRotation(0, "y", 8, 10.25, 5.5));
+
+        JsonObject faces = new JsonObject();
+        faces.add("north", createFace(1, 2, 16, 5, "#1"));
+        faces.add("east", createFace(0, 2, 15, 5, "#1"));
+        faces.add("south", createFaceWithCullface(1, 2, 15, 5, "#1", "south"));
+        faces.add("west", createFace(1, 2, 16, 5, "#1"));
+        faces.add("down", createFace(1, 1, 15, 15, "#1"));
+        element.add("faces", faces);
+
+        return element;
+    }
+
+    private static JsonObject createTableTopCenter() {
+        JsonObject element = new JsonObject();
+        element.addProperty("name", "top");
+        element.add("from", createArray(0, 14, 0));
+        element.add("to", createArray(16, 16, 16));
+        element.add("rotation", createRotation(0, "y", 8, 13.75, 8));
+
+        JsonObject faces = new JsonObject();
+        faces.add("north", createFaceWithCullface(0, 0, 16, 2, "#1", "north"));
+        faces.add("east", createFaceWithCullface(0, 0, 16, 2, "#1", "east"));
+        faces.add("south", createFaceWithCullface(0, 0, 16, 2, "#1", "south"));
+        faces.add("west", createFaceWithCullface(0, 0, 16, 2, "#1", "west"));
+        faces.add("up", createFaceWithCullface(0, 0, 16, 16, "#1", "up"));
+        faces.add("down", createFace(0, 0, 16, 16, "#1"));
+        element.add("faces", faces);
+
+        return element;
+    }
+
+    private static JsonObject createTableTopInnerCenter() {
+        JsonObject element = new JsonObject();
+        element.addProperty("name", "top");
+        element.add("from", createArray(1, 11, 0));
+        element.add("to", createArray(15, 14, 16));
+        element.add("rotation", createRotation(0, "y", 8, 13.75, 8));
+
+        JsonObject faces = new JsonObject();
+        faces.add("north", createFaceWithCullface(1, 2, 15, 5, "#1", "north"));
+        faces.add("east", createFace(0, 2, 16, 5, "#1"));
+        faces.add("south", createFaceWithCullface(1, 2, 15, 5, "#1", "south"));
+        faces.add("west", createFace(0, 2, 16, 5, "#1"));
+        faces.add("down", createFace(1, 1, 15, 15, "#1"));
+        element.add("faces", faces);
+
+        return element;
+    }
+
+    private static JsonObject createTableTopCorner() {
+        JsonObject element = new JsonObject();
+        element.addProperty("name", "top");
+        element.add("from", createArray(0, 14, 0));
+        element.add("to", createArray(16, 16, 16));
+        element.add("rotation", createRotation(0, "y", 5.5, 10.25, 5.5));
+
+        JsonObject faces = new JsonObject();
+        faces.add("north", createFaceWithCullface(0, 0, 16, 2, "#1", "north"));
+        faces.add("east", createFaceWithCullface(0, 0, 16, 2, "#1", "east"));
+        faces.add("south", createFaceWithCullface(0, 0, 16, 2, "#1", "south"));
+        faces.add("west", createFaceWithCullface(0, 0, 16, 2, "#1", "west"));
+        faces.add("up", createFaceWithCullface(0, 0, 16, 16, "#1", "up"));
+        faces.add("down", createFace(0, 0, 16, 16, "#1"));
+        element.add("faces", faces);
+
+        return element;
+    }
+
+    private static JsonObject createTableTopInnerCorner() {
+        JsonObject element = new JsonObject();
+        element.addProperty("name", "top");
+        element.add("from", createArray(1, 11, 1));
+        element.add("to", createArray(16, 14, 16));
+        element.add("rotation", createRotation(0, "y", 5.5, 10.25, 5.5));
+
+        JsonObject faces = new JsonObject();
+        faces.add("north", createFace(0, 2, 15, 5, "#1"));
+        faces.add("east", createFaceWithCullface(0, 2, 15, 5, "#1", "east"));
+        faces.add("south", createFaceWithCullface(1, 2, 16, 5, "#1", "south"));
+        faces.add("west", createFace(1, 2, 16, 5, "#1"));
+        faces.add("down", createFace(1, 0, 16, 15, "#1"));
+        element.add("faces", faces);
+
+        return element;
+    }
+
+    private static JsonObject createTableLegCorner() {
+        JsonObject element = new JsonObject();
+        element.addProperty("name", "leg");
+        element.add("from", createArray(1, 0, 1));
+        element.add("to", createArray(4, 11, 4));
+        element.add("rotation", createRotation(0, "y", 5.5, 10.25, 5.5));
+
+        JsonObject faces = new JsonObject();
+        faces.add("north", createFace(0, 5, 3, 16, "#1"));
+        faces.add("east", createFace(0, 5, 3, 16, "#1"));
+        faces.add("south", createFace(0, 5, 3, 16, "#1"));
+        faces.add("west", createFace(0, 5, 3, 16, "#1"));
+        faces.add("down", createFaceWithCullface(0, 13, 3, 16, "#1", "down"));
         element.add("faces", faces);
 
         return element;
@@ -223,6 +399,30 @@ public class TableBlockExport {
         face.add("uv", createArray(u1, v1, u2, v2));
         face.addProperty("texture", texture);
         return face;
+    }
+
+    private static JsonObject createFaceWithCullface(int u1, int v1, int u2, int v2, String texture, String cullface) {
+        JsonObject face = new JsonObject();
+        face.add("uv", createArray(u1, v1, u2, v2));
+        face.addProperty("texture", texture);
+        face.addProperty("cullface", cullface);
+        return face;
+    }
+
+    private static JsonObject createRotation(int angle, String axis, double x, double y, double z) {
+        JsonObject rotation = new JsonObject();
+        rotation.addProperty("angle", angle);
+        rotation.addProperty("axis", axis);
+        rotation.add("origin", createArray(x, y, z));
+        return rotation;
+    }
+
+    private static JsonArray createArray(double... values) {
+        JsonArray array = new JsonArray();
+        for (double value : values) {
+            array.add(value);
+        }
+        return array;
     }
 
     private static JsonArray createArray(int... values) {
