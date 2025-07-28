f1c0ebe9203644c9f1917c1ce126fbce0f433b62
jacobgranberry <jacobgranberry@gmail.com>
2025-07-28 10:34:58 -0600

    branch block - wip

--- src/main/java/com/westerosblocks/block/custom/WCBranchBlock.java
+++ src/main/java/com/westerosblocks/block/custom/WCBranchBlock.java
@@ -1,25 +1,49 @@
 package com.westerosblocks.block.custom;
 
+import com.google.common.collect.ImmutableMap;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.PillarBlock;
+import net.minecraft.block.ShapeContext;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluids;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.tooltip.TooltipType;
+import net.minecraft.registry.tag.FluidTags;
 import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.Properties;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
-import net.minecraft.block.ShapeContext;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.WorldAccess;
 
 import java.util.List;
+import java.util.Map;
 
-public class WCBranchBlock extends PillarBlock {
+public class WCBranchBlock extends Block {
+    public static final BooleanProperty NORTH = Properties.NORTH;
+    public static final BooleanProperty EAST = Properties.EAST;
+    public static final BooleanProperty SOUTH = Properties.SOUTH;
+    public static final BooleanProperty WEST = Properties.WEST;
+    public static final BooleanProperty WATERLOGGED = Properties.WATERLOGGED;
+
+    // Define base shapes for branch components
+    private static final VoxelShape BRANCH_CENTER = Block.createCuboidShape(4, 0, 4, 12, 16, 12);
+    private static final VoxelShape BRANCH_NORTH = Block.createCuboidShape(4, 8, 0, 12, 12, 8);
+    private static final VoxelShape BRANCH_EAST = Block.createCuboidShape(8, 8, 4, 16, 12, 12);
+    private static final VoxelShape BRANCH_SOUTH = Block.createCuboidShape(4, 8, 8, 12, 12, 16);
+    private static final VoxelShape BRANCH_WEST = Block.createCuboidShape(0, 8, 4, 8, 12, 12);
+
+    // Pre-computed shape maps for efficient lookups
+    private final Map<BlockState, VoxelShape> shapeByIndex;
+
     private final String blockName;
     private final String creativeTab;
     private final String woodType;
@@ -32,6 +56,61 @@ public class WCBranchBlock extends PillarBlock {
         this.creativeTab = creativeTab;
         this.woodType = woodType;
         this.branchType = branchType;
+        this.setDefaultState(this.getDefaultState()
+                .with(NORTH, false)
+                .with(EAST, false)
+                .with(SOUTH, false)
+                .with(WEST, false)
+                .with(WATERLOGGED, false));
+
+        // Pre-compute all possible shape combinations
+        this.shapeByIndex = this.makeShapes();
+    }
+
+    private Map<BlockState, VoxelShape> makeShapes() {
+        ImmutableMap.Builder<BlockState, VoxelShape> builder = ImmutableMap.builder();
+
+        // Generate all possible state combinations
+        for (boolean north : new boolean[] { false, true }) {
+            for (boolean east : new boolean[] { false, true }) {
+                for (boolean south : new boolean[] { false, true }) {
+                    for (boolean west : new boolean[] { false, true }) {
+                        VoxelShape shape = this.getShapeForConnections(north, east, south, west);
+
+                        // Add both waterlogged and non-waterlogged states
+                        BlockState state = this.getDefaultState()
+                                .with(NORTH, north)
+                                .with(EAST, east)
+                                .with(SOUTH, south)
+                                .with(WEST, west);
+
+                        builder.put(state.with(WATERLOGGED, false), shape);
+                        builder.put(state.with(WATERLOGGED, true), shape);
+                    }
+                }
+            }
+        }
+
+        return builder.build();
+    }
+
+    private VoxelShape getShapeForConnections(boolean north, boolean east, boolean south, boolean west) {
+        VoxelShape shape = BRANCH_CENTER;
+
+        if (north) {
+            shape = VoxelShapes.union(shape, BRANCH_NORTH);
+        }
+        if (east) {
+            shape = VoxelShapes.union(shape, BRANCH_EAST);
+        }
+        if (south) {
+            shape = VoxelShapes.union(shape, BRANCH_SOUTH);
+        }
+        if (west) {
+            shape = VoxelShapes.union(shape, BRANCH_WEST);
+        }
+
+        return shape;
     }
 
     public String getBlockName() {
@@ -52,7 +131,34 @@ public class WCBranchBlock extends PillarBlock {
 
     @Override
     public BlockState getPlacementState(ItemPlacementContext ctx) {
-        return this.getDefaultState().with(AXIS, ctx.getSide().getAxis());
+        FluidState fluidstate = ctx.getWorld().getFluidState(ctx.getBlockPos());
+        return this.getDefaultState()
+                .with(WATERLOGGED, fluidstate.isIn(FluidTags.WATER));
+    }
+
+    @Override
+    public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState,
+            WorldAccess world, BlockPos pos, BlockPos neighborPos) {
+        if (state.get(WATERLOGGED)) {
+            world.scheduleFluidTick(pos, Fluids.WATER, Fluids.WATER.getTickRate(world));
+        }
+
+        if (direction.getAxis().isHorizontal()) {
+            boolean isConnected = neighborState.isOf(this);
+            return state.with(getPropertyForDirection(direction), isConnected);
+        }
+
+        return super.getStateForNeighborUpdate(state, direction, neighborState, world, pos, neighborPos);
+    }
+
+    private static BooleanProperty getPropertyForDirection(Direction direction) {
+        return switch (direction) {
+            case NORTH -> NORTH;
+            case EAST -> EAST;
+            case SOUTH -> SOUTH;
+            case WEST -> WEST;
+            default -> throw new IllegalArgumentException("Invalid direction: " + direction);
+        };
     }
 
     @Override
@@ -65,15 +171,16 @@ public class WCBranchBlock extends PillarBlock {
 
     @Override
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-        builder.add(AXIS);
+        builder.add(NORTH, EAST, SOUTH, WEST, WATERLOGGED);
     }
 
     @Override
     public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
-        // Based on the model: from [4, 0, 4] to [12, 16, 12] with rotation around Y
-        // axis at [11, 7, 11]
-        // Convert from 16x16 texture coordinates to 16x16 block coordinates
-        // The model shows a rotated cube that's offset from center
-        return VoxelShapes.cuboid(0.25, 0.0, 0.25, 0.75, 1.0, 0.75);
+        return this.shapeByIndex.get(state);
+    }
+
+    @Override
+    public VoxelShape getCollisionShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return this.shapeByIndex.get(state);
     }
 }
--- src/main/java/com/westerosblocks/datagen/models/BranchBlockExport.java
+++ src/main/java/com/westerosblocks/datagen/models/BranchBlockExport.java
@@ -9,7 +9,16 @@ import net.minecraft.util.math.Direction;
 
 import java.util.Optional;
 
+/**
+ * BranchBlockExport for builder-based branch blocks with cardinal direction
+ * connections.
+ * Handles different branch models based on connection states (no connections,
+ * single connection, etc.).
+ */
 public class BranchBlockExport extends ModelExport2 {
+
+    public BranchBlockExport() {
+    }
 
     @Override
     public void generateBlockStateModels(BlockStateModelGenerator generator, Block block, String texturePath) {
@@ -24,109 +33,192 @@ public class BranchBlockExport extends ModelExport2 {
             branchType = ((WCBranchBlock) block).getBranchType();
         }
 
-        // Create models for each axis
-        Identifier xModelId = createBranchModel(generator, block, branchType, texturePaths, "x");
-        Identifier yModelId = createBranchModel(generator, block, branchType, texturePaths, "y");
-        Identifier zModelId = createBranchModel(generator, block, branchType, texturePaths, "z");
+        // Create models for different connection states
+        Identifier baseModelId = createBranchModel(generator, block, branchType, texturePaths, "base");
+        Identifier connectedModelId = createBranchModel(generator, block, branchType, texturePaths, "connected");
 
-        // Create block state variants using BlockStateVariantMap with the AXIS property
-        BlockStateVariantMap variants = BlockStateVariantMap.create(WCBranchBlock.AXIS)
-                .register(Direction.Axis.X, BlockStateVariant.create()
-                        .put(VariantSettings.MODEL, xModelId)
-                        .put(VariantSettings.X, VariantSettings.Rotation.R90)
-                        .put(VariantSettings.Y, VariantSettings.Rotation.R90))
-                .register(Direction.Axis.Y, createVariant(yModelId))
-                .register(Direction.Axis.Z, BlockStateVariant.create()
-                        .put(VariantSettings.MODEL, zModelId)
-                        .put(VariantSettings.X, VariantSettings.Rotation.R90));
+        // Use MultipartBlockStateSupplier for more efficient state generation
+        MultipartBlockStateSupplier stateSupplier = MultipartBlockStateSupplier.create(block);
 
-        // Register the block state using the utility method
-        registerBlockState(generator, block, variants);
+        // Single branch (no connections)
+        When.PropertyCondition singleCondition = When.create()
+                .set(WCBranchBlock.NORTH, false)
+                .set(WCBranchBlock.EAST, false)
+                .set(WCBranchBlock.SOUTH, false)
+                .set(WCBranchBlock.WEST, false);
+        stateSupplier.with(singleCondition, createVariant(baseModelId));
+
+        // Single connection variants
+        // North connection
+        When.PropertyCondition northCondition = When.create()
+                .set(WCBranchBlock.NORTH, true)
+                .set(WCBranchBlock.EAST, false)
+                .set(WCBranchBlock.SOUTH, false)
+                .set(WCBranchBlock.WEST, false);
+        stateSupplier.with(northCondition, createVariant(connectedModelId));
+
+        // South connection
+        When.PropertyCondition southCondition = When.create()
+                .set(WCBranchBlock.NORTH, false)
+                .set(WCBranchBlock.EAST, false)
+                .set(WCBranchBlock.SOUTH, true)
+                .set(WCBranchBlock.WEST, false);
+        stateSupplier.with(southCondition, createVariant(connectedModelId, 180));
+
+        // East connection
+        When.PropertyCondition eastCondition = When.create()
+                .set(WCBranchBlock.NORTH, false)
+                .set(WCBranchBlock.EAST, true)
+                .set(WCBranchBlock.SOUTH, false)
+                .set(WCBranchBlock.WEST, false);
+        stateSupplier.with(eastCondition, createVariant(connectedModelId, 90));
+
+        // West connection
+        When.PropertyCondition westCondition = When.create()
+                .set(WCBranchBlock.NORTH, false)
+                .set(WCBranchBlock.EAST, false)
+                .set(WCBranchBlock.SOUTH, false)
+                .set(WCBranchBlock.WEST, true);
+        stateSupplier.with(westCondition, createVariant(connectedModelId, 270));
+
+        // Two adjacent connections (corner variants)
+        // North-West corner
+        When.PropertyCondition northWestCondition = When.create()
+                .set(WCBranchBlock.NORTH, true)
+                .set(WCBranchBlock.EAST, false)
+                .set(WCBranchBlock.SOUTH, false)
+                .set(WCBranchBlock.WEST, true);
+        stateSupplier.with(northWestCondition, createVariant(connectedModelId, 270));
+
+        // North-East corner
+        When.PropertyCondition northEastCondition = When.create()
+                .set(WCBranchBlock.NORTH, true)
+                .set(WCBranchBlock.EAST, true)
+                .set(WCBranchBlock.SOUTH, false)
+                .set(WCBranchBlock.WEST, false);
+        stateSupplier.with(northEastCondition, createVariant(connectedModelId, 90));
+
+        // South-West corner
+        When.PropertyCondition southWestCondition = When.create()
+                .set(WCBranchBlock.NORTH, false)
+                .set(WCBranchBlock.EAST, false)
+                .set(WCBranchBlock.SOUTH, true)
+                .set(WCBranchBlock.WEST, true);
+        stateSupplier.with(southWestCondition, createVariant(connectedModelId, 180));
+
+        // South-East corner
+        When.PropertyCondition southEastCondition = When.create()
+                .set(WCBranchBlock.NORTH, false)
+                .set(WCBranchBlock.EAST, true)
+                .set(WCBranchBlock.SOUTH, true)
+                .set(WCBranchBlock.WEST, false);
+        stateSupplier.with(southEastCondition, createVariant(connectedModelId, 180));
+
+        // Opposite connections (center variants)
+        // North-South center
+        When.PropertyCondition northSouthCondition = When.create()
+                .set(WCBranchBlock.NORTH, true)
+                .set(WCBranchBlock.EAST, false)
+                .set(WCBranchBlock.SOUTH, true)
+                .set(WCBranchBlock.WEST, false);
+        stateSupplier.with(northSouthCondition, createVariant(connectedModelId, 180));
+
+        // East-West center
+        When.PropertyCondition eastWestCondition = When.create()
+                .set(WCBranchBlock.NORTH, false)
+                .set(WCBranchBlock.EAST, true)
+                .set(WCBranchBlock.SOUTH, false)
+                .set(WCBranchBlock.WEST, true);
+        stateSupplier.with(eastWestCondition, createVariant(connectedModelId, 90));
+
+        // Three connections
+        // North-East-West
+        When.PropertyCondition northEastWestCondition = When.create()
+                .set(WCBranchBlock.NORTH, true)
+                .set(WCBranchBlock.EAST, true)
+                .set(WCBranchBlock.SOUTH, false)
+                .set(WCBranchBlock.WEST, true);
+        stateSupplier.with(northEastWestCondition, createVariant(connectedModelId, 90));
+
+        // North-East-South
+        When.PropertyCondition northEastSouthCondition = When.create()
+                .set(WCBranchBlock.NORTH, true)
+                .set(WCBranchBlock.EAST, true)
+                .set(WCBranchBlock.SOUTH, true)
+                .set(WCBranchBlock.WEST, false);
+        stateSupplier.with(northEastSouthCondition, createVariant(connectedModelId, 180));
+
+        // North-South-West
+        When.PropertyCondition northSouthWestCondition = When.create()
+                .set(WCBranchBlock.NORTH, true)
+                .set(WCBranchBlock.EAST, false)
+                .set(WCBranchBlock.SOUTH, true)
+                .set(WCBranchBlock.WEST, true);
+        stateSupplier.with(northSouthWestCondition, createVariant(connectedModelId, 180));
+
+        // East-South-West
+        When.PropertyCondition eastSouthWestCondition = When.create()
+                .set(WCBranchBlock.NORTH, false)
+                .set(WCBranchBlock.EAST, true)
+                .set(WCBranchBlock.SOUTH, true)
+                .set(WCBranchBlock.WEST, true);
+        stateSupplier.with(eastSouthWestCondition, createVariant(connectedModelId, 90));
+
+        // All connections
+        When.PropertyCondition allConnectionsCondition = When.create()
+                .set(WCBranchBlock.NORTH, true)
+                .set(WCBranchBlock.EAST, true)
+                .set(WCBranchBlock.SOUTH, true)
+                .set(WCBranchBlock.WEST, true);
+        stateSupplier.with(allConnectionsCondition, createVariant(connectedModelId, 180));
+
+        // Register the block state using the utility method from ModelExport2
+        registerMultipartBlockState(generator, stateSupplier);
     }
 
     private Identifier createBranchModel(BlockStateModelGenerator generator, Block block, String branchType,
-            String[] texturePaths, String axis) {
-        // Expand texturePaths to 6 items by filling remaining slots with the last
-        // texture
-        String[] expandedTextures = expandTextureArray(texturePaths);
+            String[] texturePaths, String connectionType) {
+        // Use the first texture for all faces (branches typically use single texture)
+        String texturePath = texturePaths.length > 0 ? texturePaths[0] : "oak_branch";
 
-        // Map textures to faces: [bottom, top, north, south, west, east]
-        String bottomTexture = expandedTextures[0]; // down
-        String topTexture = expandedTextures[1]; // up
-        String northTexture = expandedTextures[2]; // north
-        String southTexture = expandedTextures[3]; // south
-        String westTexture = expandedTextures[4]; // west
-        String eastTexture = expandedTextures[5]; // east
-        String particleTexture = expandedTextures[2]; // Use north texture for particle
-
-        // Create texture map with proper mapping for each face
+        // Create texture map
         TextureMap textureMap = new TextureMap()
-                .put(TextureKey.DOWN, createBlockIdentifier(bottomTexture))
-                .put(TextureKey.UP, createBlockIdentifier(topTexture))
-                .put(TextureKey.NORTH, createBlockIdentifier(northTexture))
-                .put(TextureKey.SOUTH, createBlockIdentifier(southTexture))
-                .put(TextureKey.WEST, createBlockIdentifier(westTexture))
-                .put(TextureKey.EAST, createBlockIdentifier(eastTexture))
-                .put(TextureKey.PARTICLE, createBlockIdentifier(particleTexture));
+                .put(TextureKey.ALL, createBlockIdentifier(texturePath))
+                .put(TextureKey.PARTICLE, createBlockIdentifier(texturePath));
 
-        Identifier modelId = WesterosBlocks.id(
-                "block/generated/" + block.getTranslationKey().replace("block.westerosblocks.", "") + "/" + axis);
-
-        // Use a custom model template for branches, or fall back to log template
+        // Determine model template based on connection type
         Optional<Identifier> parentModel = Optional.empty();
         if ("large_branch".equals(branchType)) {
-            parentModel = Optional.of(WesterosBlocks.id("block/custom/branches/large_branch"));
+            switch (connectionType) {
+                case "base":
+                    parentModel = Optional.of(WesterosBlocks.id("block/custom/branches/large_branch"));
+                    break;
+                case "connected":
+                    parentModel = Optional.of(WesterosBlocks.id("block/custom/branches/large_branch_connected"));
+                    break;
+                default:
+                    parentModel = Optional.of(WesterosBlocks.id("block/custom/branches/large_branch"));
+            }
         } else {
-            parentModel = Optional.of(WesterosBlocks.id("block/untinted/cube_log")); // Default to log template
+            // Default to base branch model for other branch types
+            parentModel = Optional.of(WesterosBlocks.id("block/custom/branches/large_branch"));
         }
-        // Add more branch type specific templates here as needed
 
-        Model model = new Model(parentModel, Optional.empty(),
-                TextureKey.DOWN, TextureKey.UP, TextureKey.NORTH, TextureKey.SOUTH, TextureKey.WEST, TextureKey.EAST,
-                TextureKey.PARTICLE);
+        Identifier modelId = WesterosBlocks.id(
+                "block/generated/" + block.getTranslationKey().replace("block.westerosblocks.", "") + "/"
+                        + connectionType);
+
+        Model model = new Model(parentModel, Optional.empty(), TextureKey.ALL, TextureKey.PARTICLE);
         model.upload(modelId, textureMap, generator.modelCollector);
 
         return modelId;
     }
 
-    /**
-     * Expands a texture array to exactly 6 items by filling remaining slots with
-     * the last texture.
-     * If the input array has fewer than 6 items, the last item is used to fill the
-     * remaining slots.
-     * 
-     * @param texturePaths The input texture array
-     * @return An array with exactly 6 texture paths
-     */
-    private String[] expandTextureArray(String... texturePaths) {
-        if (texturePaths == null || texturePaths.length == 0) {
-            // Return default textures if none provided
-            return new String[] { "oak_branch", "oak_branch", "oak_branch", "oak_branch", "oak_branch", "oak_branch" };
-        }
-
-        if (texturePaths.length >= 6) {
-            // Already has 6 or more textures, return first 6
-            String[] result = new String[6];
-            System.arraycopy(texturePaths, 0, result, 0, 6);
-            return result;
-        }
-
-        // Expand to 6 textures by filling remaining slots with the last texture
-        String[] result = new String[6];
-        System.arraycopy(texturePaths, 0, result, 0, texturePaths.length);
-
-        String lastTexture = texturePaths[texturePaths.length - 1];
-        for (int i = texturePaths.length; i < 6; i++) {
-            result[i] = lastTexture;
-        }
-
-        return result;
-    }
-
     @Override
     public void generateItemModels(ItemModelGenerator generator, Block block) {
-        // Use the vertical (Y-axis) model for the item
-        String modelPath = "block/generated/" + block.getTranslationKey().replace("block.westerosblocks.", "") + "/y";
+        // Use the base model for the item
+        String modelPath = "block/generated/" + block.getTranslationKey().replace("block.westerosblocks.", "")
+                + "/base";
         Model model = new Model(
                 Optional.of(Identifier.of("westerosblocks", modelPath)),
                 Optional.empty());
