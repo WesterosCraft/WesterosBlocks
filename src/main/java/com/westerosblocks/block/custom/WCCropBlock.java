f97ad338fa6d350a1b8b391dfd93a2344ee5991b
jacobgranberry <jacobgranberry@gmail.com>
2025-09-13 08:53:20 -0700

    fix block params

--- src/main/java/com/westerosblocks/block/custom/WCCropBlock.java
+++ src/main/java/com/westerosblocks/block/custom/WCCropBlock.java
@@ -3,6 +3,7 @@ package com.westerosblocks.block.custom;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.state.property.Properties;
+import java.util.Map;
 
 public class WCCropBlock extends WCPlantBlock {
     
@@ -10,8 +11,21 @@ public class WCCropBlock extends WCPlantBlock {
         @Override
         public Block buildBlockClass(AbstractBlock.Settings settings, Object... params) {
             AbstractBlock.Settings props = settings.noCollision().strength(0.0f);
-            boolean layerSensitive = params.length > 0 ? (Boolean) params[0] : false;
-            boolean toggleOnUse = params.length > 1 ? (Boolean) params[1] : false;
+            
+            boolean layerSensitive = false;
+            boolean toggleOnUse = false;
+            
+            if (params.length > 0 && params[0] instanceof Map) {
+                @SuppressWarnings("unchecked")
+                Map<String, Object> paramMap = (Map<String, Object>) params[0];
+                layerSensitive = (Boolean) paramMap.getOrDefault("layerSensitive", false);
+                toggleOnUse = (Boolean) paramMap.getOrDefault("toggleOnUse", false);
+            } else {
+                // Legacy parameter handling for backward compatibility
+                layerSensitive = params.length > 0 ? (Boolean) params[0] : false;
+                toggleOnUse = params.length > 1 ? (Boolean) params[1] : false;
+            }
+            
             if (layerSensitive) {
                 tempLAYERS = Properties.LAYERS;
             }
