61ba34ee5c3a9f712288f909c6a3f922a3fc6b8d
jacobgranberry <jacobgranberry@gmail.com>
2025-11-01 14:24:29 -0700

    add table

--- src/main/java/com/westerosblocks/datagen/ModTextureMap.java
+++ src/main/java/com/westerosblocks/datagen/ModTextureMap.java
@@ -1,14 +1,10 @@
 package com.westerosblocks.datagen;
 
-import net.minecraft.block.Block;
 import net.minecraft.data.client.TextureKey;
 import net.minecraft.data.client.TextureMap;
 import net.minecraft.util.Identifier;
 
 public class ModTextureMap {
-    public static TextureMap customAll(Block block, String suffix) {
-        return new TextureMap().put(TextureKey.ALL, Identifier.of("westerosblocks", "block/" + suffix));
-    }
 
     /**
      * Creates a texture map for all six sides of a block with custom textures
@@ -40,33 +36,5 @@ public class ModTextureMap {
                 .put(TextureKey.EAST, Identifier.of("westerosblocks", "block/" + filledTextures[4]))
                 .put(TextureKey.WEST, Identifier.of("westerosblocks", "block/" + filledTextures[5]))
                 .put(TextureKey.PARTICLE, Identifier.of("westerosblocks", "block/" + filledTextures[0]));
-    }
-
-    /**
-     * Creates a texture map specifically for slab models
-     * 
-     * @param textures Array of texture paths in order: bottom, top, side
-     * @return TextureMap with bottom, top, and side keys configured
-     */
-    public static TextureMap customSlab(String... textures) {
-        if (textures.length == 0) {
-            throw new IllegalArgumentException("At least one texture path is required");
-        }
-
-        // Fill remaining slots with the last texture if less than 3 provided
-        String[] filledTextures = new String[3];
-        for (int i = 0; i < 3; i++) {
-            if (i < textures.length) {
-                filledTextures[i] = textures[i];
-            } else {
-                filledTextures[i] = textures[textures.length - 1];
-            }
-        }
-
-        return new TextureMap()
-                .put(TextureKey.BOTTOM, Identifier.of("westerosblocks", "block/" + filledTextures[0]))
-                .put(TextureKey.TOP, Identifier.of("westerosblocks", "block/" + filledTextures[1]))
-                .put(TextureKey.SIDE, Identifier.of("westerosblocks", "block/" + filledTextures[2]))
-                .put(TextureKey.PARTICLE, Identifier.of("westerosblocks", "block/" + filledTextures[0])); // particle
     }
 }
