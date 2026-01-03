4f074bf62ee89958250e88d96638faa27607ae66
jacobgranberry <jacobgranberry@gmail.com>
2026-01-03 12:36:02 -0800

    fix tinted solid blocks

--- src/main/java/com/westerosblocks/datagen/custom/SolidBlockExporter.java
+++ src/main/java/com/westerosblocks/datagen/custom/SolidBlockExporter.java
@@ -36,7 +36,7 @@ public class SolidBlockExporter extends BaseBlockExporter {
             BlockDefinition.StateVariant state = states.get(stateIdx);
             String stateID = state.getStateID();
             String fname = (stateID == null) ? "base" : stateID;
-            boolean isTinted = definition.isTinted();
+            boolean isTinted = definition.isTinted() || definition.hasColorMult();
             boolean isOverlay = state.hasOverlayTextures();
 
             for (int setIdx = 0; setIdx < state.getRandomTextureSetCount(); setIdx++) {
